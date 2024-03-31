18f59c6e3eb2464a02a8c218840fdd8f3cb66eef
Ian Holloway <ian.m.holloway@gmail.com>
2024-03-30 23:16:47 -0700

    setup nixos configs for future work

--- hosts/desktop/default.nix
+++ hosts/desktop/default.nix
@@ -20,21 +20,54 @@ lib.cust.mkHost {
   };
 
   nixosModules = with tree.hosts.shared; [
-    # Base
-    nix-settings
+    ## Base
+    base.nix-settings
+    # nix-registry
+    ./users.nix
+    # xdg
 
-    # Boot
+    ## Boot
     boot.grub.default
     boot.grub.dual-boot
+    # kernel modules
+    # plymouth
+    # grub theme
 
-    # Hardware
-    zram
+    ## Loclaization
+    # time
+    # keyboard
+    # language
 
-    # Desktop Environments
-    (desktop-envs.hyprland {useMainUser = true;})
-
+    ## Hardware
     ./filesystems.nix
-    ./users.nix
+    zram
+    # networking
+    # virtualization
+    # bluetooth
+    # firmware
+    # pipewire
+    # GPU
+    # CPU
+    # power management
+    # pheripherals (mouse, trackpad, etc)
+    # gaming support
+
+    ## Input
+    # fonts
+    # fcitx
+
+    ## Desktop Environments
+    (desktop-envs.hyprland {useMainUser = true;})
+    # gnome
+    # plasma
+
+    ## Display Manager
+    # greetd
+    # gdm
+    # sddm
+
+    # Other
+    # swaylock setup (replace with hyprlock)
   ];
 
   overlays = [];
