062b03ee684f2679451b86e0e65a20e9eb875daa
Ian Holloway <ian.m.holloway@gmail.com>
2024-04-18 16:21:41 -0700

    Add gpu vars to fix hyprland
    * right now this is a fix as home manager doesn't seem to start early enough to apply the vars

--- hosts/desktop/hardware/gpu.nix
+++ hosts/desktop/hardware/gpu.nix
@@ -18,4 +18,11 @@
 
     nvidiaSettings = true;
   };
+
+  environment.sessionVariables = {
+    LIBVA_DRIVER_NAME = "nvidia";
+    GBM_BACKEND = "nvidia-drm"; # If you encounter crashes in Firefox then remove this line
+    __GLX_VENDOR_LIBRARY_NAME = "nvidia"; # If you face problems with Discord windows not displaying or screen sharing not working in Zoom then remove this line
+    WLR_NO_HARDWARE_CURSORS = "1";
+  };
 }
