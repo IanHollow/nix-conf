31c12e219495ec8c5a9b2487052bf10617fb0403
Ian Holloway <ian.m.holloway@gmail.com>
2024-04-23 14:21:01 -0700

    change gpu to config to module

--- hosts/desktop/hardware/gpu.nix
+++ hosts/desktop/hardware/gpu.nix
@@ -17,12 +17,7 @@
     earlyLoading = true;
 
     nvidiaSettings = true;
-  };
 
-  environment.sessionVariables = {
-    LIBVA_DRIVER_NAME = "nvidia";
-    GBM_BACKEND = "nvidia-drm"; # If you encounter crashes in Firefox then remove this line
-    __GLX_VENDOR_LIBRARY_NAME = "nvidia"; # If you face problems with Discord windows not displaying or screen sharing not working in Zoom then remove this line
-    WLR_NO_HARDWARE_CURSORS = "1";
+    waylandEnvs = true;
   };
 }
