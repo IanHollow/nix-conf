0626a18fbdf811d204b54acce4f360179ca059bb
Ian Holloway <ian.m.holloway@gmail.com>
2024-04-27 16:02:38 -0700

    added amd cpu iommu kernel params

--- _nixosModules/hardware/cpu/amd.nix
+++ _nixosModules/hardware/cpu/amd.nix
@@ -5,10 +5,16 @@ in
 {
   options.hardware.cpu.amd = {
     enable = lib.mkEnableOption "Enable support for AMD CPUs.";
+    iommu = lib.mkEnableOption "Enable IOMMU support.";
   };
 
   config = lib.mkIf cfg.enable {
     hardware.enableRedistributableFirmware = lib.mkDefault true;
     hardware.cpu.amd.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
+
+    boot.kernelParams = lib.mkIf cfg.iommu [
+      "amd_iommu=on" # enable IOMMU support
+      "iommu=pt" # prevent Linux from touching devices which cannot be passed through
+    ];
   };
 }
