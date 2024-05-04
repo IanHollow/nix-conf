5cef1820f746ab2ef21b6ccd0044ff6f6067739d
Ian Holloway <ian.m.holloway@gmail.com>
2024-05-03 18:01:52 -0700

    added new sources to flake

--- flake.nix
+++ flake.nix
@@ -58,17 +58,30 @@
       url = "github:the-argus/spicetify-nix";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+
+    # Nix gaming
+    nix-gaming = {
+      url = "github:fufexan/nix-gaming";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    # Wayland Packages
+    nixpkgs-wayland.url = "github:nix-community/nixpkgs-wayland";
   };
 
   nixConfig = {
     # instances of cachix for package derivations
     extra-substituters = [
+      "https://cache.nixos.org"
       "https://nix-community.cachix.org"
       "https://hyprland.cachix.org"
+      "https://nixpkgs-wayland.cachix.org"
     ];
     extra-trusted-public-keys = [
+      "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
       "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
+      "nixpkgs-wayland.cachix.org-1:3lwxaILxMRkVhehr5StQprHdEo4IrE8sRho9R9HOLYA="
     ];
   };
 
