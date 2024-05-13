5e7137fc0ee4fc55d3dae0dc70dac8306588c5a2
Ian Holloway <ian.m.holloway@gmail.com>
2024-05-12 17:07:54 -0700

    update sddm

--- hosts/shared/display-managers/sddm.nix
+++ hosts/shared/display-managers/sddm.nix
@@ -1,8 +1,11 @@
+{ pkgs, ... }:
 {
   services = {
     displayManager.sddm = {
       enable = true;
       wayland.enable = true;
+      wayland.compositor = "kwin";
+      package = pkgs.kdePackages.sddm;
     };
   };
 }
