2827fa97ea86dd102a79f4f4f46ca68eefd5d262
Ian Holloway <ian.m.holloway@gmail.com>
2024-05-18 11:57:05 -0700

    apply fix for direnv in vscode

--- home/configs/shared/programs/dev/tools/direnv.nix
+++ home/configs/shared/programs/dev/tools/direnv.nix
@@ -1,9 +1,46 @@
-{inputs,pkgs,...}:{
+{
+  inputs,
+  pkgs,
+  config,
+  lib,
+  ...
+}:
+{
   programs.direnv = {
     enable = true;
     enableBashIntegration = true;
     enableZshIntegration = true;
     nix-direnv.enable = true;
     nix-direnv.package = inputs.nix-direnv.packages.${pkgs.system}.default;
+  };
+
+  home.file."${config.home.homeDirectory}/.zshrc-direnv-vscode-fix".text = ''
+    # This is a workaround to make direnv work with VS Code's integrated terminal
+    # when using the direnv extension, by making sure to reload
+    # the environment the first time terminal is opened.
+    #
+    # See https://github.com/direnv/direnv-vscode/issues/561#issuecomment-1837462994.
+    #
+    # The variable VSCODE_INJECTION is apparently set by VS Code itself, and this is how
+    # we can detect if we're running inside the VS Code terminal or not.
+    if [[ -n "$VSCODE_INJECTION" && -z "$VSCODE_TERMINAL_DIRENV_LOADED" && -f .envrc ]]; then
+        cd ..
+        cd -
+        export VSCODE_TERMINAL_DIRENV_LOADED=1
+        clear
+    fi
+  '';
+
+  home.activation = {
+    removeZshBackup = lib.hm.dag.entryBefore [ "checkLinkTargets" ] ''
+      rm -f ${config.home.homeDirectory}/.zshrc.backup
+    '';
+    zshrcDirenvVscodeFix = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
+      rm -f ${config.home.homeDirectory}/.zshrc-temp
+      mv ${config.home.homeDirectory}/.zshrc ${config.home.homeDirectory}/.zshrc-temp
+      cat ${config.home.homeDirectory}/.zshrc-temp ${config.home.homeDirectory}/.zshrc-direnv-vscode-fix > ${config.home.homeDirectory}/.zshrc
+      chmod 444 ${config.home.homeDirectory}/.zshrc
+      rm -f ${config.home.homeDirectory}/.zshrc-temp ${config.home.homeDirectory}/.zshrc.backup
+    '';
   };
 }
--- home/configs/shared/programs/zsh/default.nix
+++ home/configs/shared/programs/zsh/default.nix
@@ -44,19 +44,6 @@ in
     initExtra = ''
       # Powerlevel10k Configuration
       [[ ! -f ${p10k_config} ]] || source ${p10k_config}
-
-      # # VSCode Shell Integration
-      # [[ "$TERM_PROGRAM" == "vscode" ]] && . "$(NIXOS_OZONE_WL= ${lib.getExe config.programs.vscode.package} --locate-shell-integration-path zsh)"
-
-      # # This is a workaround to make direnv work with VS Code's integrated terminal
-      # # when using the direnv extension, by making sure to reload
-      # # the environment the first time terminal is opened.
-      # #
-      # # See https://github.com/direnv/direnv-vscode/issues/561#issuecomment-1837462994.
-      # if [[ -n "$VSCODE_SHELL_INTEGRATION" && -f .envrc ]]; then
-      #     direnv reload
-      #     clear
-      # fi
     '';
   };
 
