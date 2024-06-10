6edbe2b730d484e5fcb2aa5038b62e6f6856de90
Ian Holloway <ian.m.holloway@gmail.com>
2024-06-10 11:17:09 -0700

    add mesa fix to hyprland

--- hosts/shared/desktop-envs/hyprland.nix
+++ hosts/shared/desktop-envs/hyprland.nix
@@ -34,4 +34,16 @@ in
       package = hyprlandPkg;
     })
   ];
+
+  hardware.opengl =
+    let
+      pkgs-unstable = inputs.hyprland.inputs.nixpkgs.legacyPackages.${pkgs.stdenv.hostPlatform.system};
+    in
+    {
+      package = pkgs-unstable.mesa.drivers;
+
+      # if you also want 32-bit support (e.g for Steam)
+      driSupport32Bit = true;
+      package32 = pkgs-unstable.pkgsi686Linux.mesa.drivers;
+    };
 }
