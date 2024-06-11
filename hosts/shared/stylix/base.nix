44dccab1fb5a2a4efea52225128c927687a5d22f
Ian Holloway <ian.m.holloway@gmail.com>
2024-06-10 17:27:06 -0700

    switch to non flake hyprland

--- hosts/shared/stylix/base.nix
+++ hosts/shared/stylix/base.nix
@@ -1,7 +1,7 @@
 { inputs, pkgs, ... }:
 {
   imports = [ inputs.stylix.nixosModules.stylix ];
-  stylix.image = "${inputs.hyprland.packages.${pkgs.system}.hyprland}/share/hyprland/wall0.png";
+  stylix.image = ./background.jpg;
   stylix.base16Scheme = "${pkgs.base16-schemes}/share/themes/pop.yaml";
 
   stylix.polarity = "dark";
