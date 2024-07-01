8a2c765c6d0d179ca3a0d549f0c4fd8714c0f1b3
Ian Holloway <ian.m.holloway@gmail.com>
2024-07-01 10:45:15 -0700

    refactor: Update NixOS hardware CPU modules to enable IOMMU and KVM support

--- _nixOSModules/hardware/cpu/intel.nix
+++ _nixOSModules/hardware/cpu/intel.nix
@@ -5,16 +5,29 @@ in
 {
   options.hardware.cpu.intel = {
     enable = lib.mkEnableOption "Enable support for Intel CPUs.";
-    iommu = lib.mkEnableOption "Enable IOMMU support.";
+    iommu.enable = lib.mkEnableOption "Enable IOMMU support.";
+    kvm = {
+      enable = lib.mkEnableOption "Enable KVM support.";
+      nestedVirtualization = lib.mkEnableOption "Enable nested virtualization.";
+    };
   };
 
   config = lib.mkIf cfg.enable {
     hardware.enableRedistributableFirmware = lib.mkDefault true;
     hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
 
-    boot.kernelParams = lib.mkIf cfg.iommu [
-      "intel_iommu=on" # enable IOMMU support
-      "iommu=pt" # prevent Linux from touching devices which cannot be passed through
-    ];
+    boot.kernelParams =
+      [ ]
+      ++ lib.optionals cfg.iommu.enable [
+        "intel_iommu=on" # enable IOMMU support
+        "iommu=pt" # prevent Linux from touching devices which cannot be passed through
+      ]
+      ++ lib.optionals cfg.kvm.enable [
+        "kvm-intel" # enable KVM support
+      ];
+
+    boot.extraModprobeConfig = lib.mkIf (
+      cfg.kvm.enable && cfg.kvm.nestedVirtualization
+    ) "options kvm_amd nested=1";
   };
 }
