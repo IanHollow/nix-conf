aaae29b8ca9c0eb48d4f9d6a4f305c6d18791eb6
Ian Holloway <ian.m.holloway@gmail.com>
2024-07-01 11:00:01 -0700

    update bluetooth

--- hosts/shared/hardware/bluetooth.nix
+++ hosts/shared/hardware/bluetooth.nix
@@ -1,12 +1,18 @@
 {
+  boot.kernelParams = [ "btusb" ];
+
   hardware.bluetooth = {
     enable = true;
     powerOnBoot = true;
+    disabledPlugins = [ "sap" ];
 
     settings = {
       General = {
         Enable = "Source,Sink,Media,Socket";
         ControllerMode = "dual";
+
+        JustWorksRepairing = "always";
+        MultiProfile = "multiple";
       };
     };
   };
