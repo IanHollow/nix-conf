7e5f5dce3b7be9ffa5d3e3018c976118bc6f973a
Ian Holloway <ian.m.holloway@gmail.com>
2024-07-01 11:02:52 -0700

    change what is enabled for desktop

--- hosts/desktop/default.nix
+++ hosts/desktop/default.nix
@@ -5,81 +5,90 @@
   inputs,
   ...
 }:
-{
+rec {
   system = "x86_64-linux";
   nixpkgsArgs = {
     config.allowUnfree = true;
   };
-  modules =
-    with (tree.hosts.${hostname} // tree.hosts.${hostname}.hardware // tree.hosts.shared);
-    [
-      ## Base
-      base.nix-settings
-      base.nix-registry
-      base.base
-      base.kernel
-      base.packages
-      users
+  modules = with (tree.hosts.${hostname} // tree.hosts.${hostname}.hardware // tree.hosts.shared); [
+    ## Base
+    base.nix-settings
+    base.nix-registry
+    base.base
+    base.kernel
+    base.packages
+    users
 
-      ## Boot
-      boot.grub.default
-      boot.grub.dual-boot
-      kernel-modules
-      # plymouth
+    ## Boot
+    boot.generic
+    boot.grub.default
+    boot.grub.dual-boot
+    kernel-modules
+    # plymouth
 
-      ## Locale
-      timezone
-      locale.timesync
-      locale.fonts
-      # keyboard
+    ## Locale
+    timezone
+    locale.timesync
+    locale.fonts
+    # keyboard
 
-      ## Hardware
-      filesystems
-      gpu
-      cpu
-      power
-      hardware.zram
-      hardware.networking
-      # virtualization
-      hardware.bluetooth
-      # firmware
-      hardware.pipewire
-      peripherals.mouse
-      gaming.default
+    ## Hardware
+    filesystems
+    gpu
+    cpu
+    power
+    audio
+    hardware.zram
+    hardware.networking
+    hardware.bluetooth
+    hardware.tpm
+    hardware.firmware
+    hardware.sound.default
+    hardware.ssd
+    peripherals.mouse
+    gaming.default
 
-      ## Desktop Environments
+    ## Virtualization
+    virtualisation.docker
+    virtualisation.libvirt
 
-      # desktop-envs.gnome
-      # desktop-envs.plasma
-      desktop-envs.hyprland
-      ## Display Managers
-      # display-managers.greetd
-      display-managers.gdm
-      # display-managers.sddm
+    ## Desktop Environments
+    # desktop-envs.gnome
+    # desktop-envs.plasma
+    desktop-envs.hyprland
 
-      ## Services
-      services.disable-hibernate
+    ## Display Managers
+    # display-managers.greetd
+    display-managers.gdm
+    # display-managers.sddm
 
-      ## Theming
-      stylix.base
-      stylix.cursor
-      stylix.fonts
+    ## Security
+    security.default
 
-      # Other
-      # swaylock setup (replace with hyprlock)
-      # keyring
-      # polkit
+    ## Services
+    services.disable-hibernate
 
-    ]
-    ++ [
-      ## Environment Variables
-      { environment.sessionVariables = lib.cust.env.wayland.all; }
-    ];
+    ## Theming
+    stylix.base
+    stylix.cursor
+    stylix.fonts
+
+    # Other
+    # keyring
+
+    # server.openssh
+
+    # secrets.agenix
+
+    server.minecraft.default
+
+    ## Environment Variables
+    { environment.sessionVariables = lib.cust.env.wayland.all; }
+  ];
 
   overlays = [
     inputs.nur.overlay
     inputs.vscode-extensions.overlays.default
     inputs.nixd.overlays.default
   ];
-
 }
