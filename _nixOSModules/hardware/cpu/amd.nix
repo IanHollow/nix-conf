af40bf6d04f007d8a8cad5c09cbc6136f75e0fdd
Ian Holloway <ian.m.holloway@gmail.com>
2024-07-01 10:45:15 -0700

    refactor: Update NixOS hardware CPU modules to enable IOMMU and KVM support

--- _nixOSModules/hardware/cpu/amd.nix
+++ _nixOSModules/hardware/cpu/amd.nix
@@ -5,16 +5,34 @@ in
 {
   options.hardware.cpu.amd = {
     enable = lib.mkEnableOption "Enable support for AMD CPUs.";
-    iommu = lib.mkEnableOption "Enable IOMMU support.";
+    iommu.enable = lib.mkEnableOption "Enable IOMMU support.";
+    kvm = {
+      enable = lib.mkEnableOption "Enable KVM support.";
+      nestedVirtualization = lib.mkEnableOption "Enable nested virtualization.";
+    };
   };
 
   config = lib.mkIf cfg.enable {
     hardware.enableRedistributableFirmware = lib.mkDefault true;
     hardware.cpu.amd.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
 
-    boot.kernelParams = lib.mkIf cfg.iommu [
-      "amd_iommu=on" # enable IOMMU support
-      "iommu=pt" # prevent Linux from touching devices which cannot be passed through
-    ];
+    boot.kernelParams =
+      [
+        "amd-pstate" # load pstate module in case the device has a newer gpu
+        "zenpower" # zenpower is for reading cpu info, i.e voltage
+        "msr" # x86 CPU MSR access device
+      ]
+      ++ lib.optionals cfg.iommu.enable [
+        "amd_iommu=on" # enable IOMMU support
+        "iommu=pt" # prevent Linux from touching devices which cannot be passed through
+      ]
+      ++ lib.optionals cfg.kvm.enable [
+        "kvm-amd" # enable KVM support
+      ];
+
+    boot.extraModprobeConfig = lib.mkIf (
+      cfg.kvm.enable && cfg.kvm.nestedVirtualization
+    ) "options kvm_amd nested=1";
+
   };
 }
