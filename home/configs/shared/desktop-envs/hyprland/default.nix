b463694bd9e130a2fa112929df15e59b59f70dcc
Ian Holloway <ian.m.holloway@gmail.com>
2024-07-01 10:47:53 -0700

    removed unkown commands

--- home/configs/shared/desktop-envs/hyprland/default.nix
+++ home/configs/shared/desktop-envs/hyprland/default.nix
@@ -1,11 +1,4 @@
-{
-  self,
-  config,
-  lib,
-  pkgs,
-  inputs,
-  ...
-}:
+{ inputs, ... }:
 {
   imports = [
     inputs.hyprland.homeManagerModules.default
@@ -15,17 +8,5 @@
     ./xdg.nix
   ];
 
-  home.packages = [
-    # xwayland perm for pkexec
-    pkgs.xorg.xhost
-  ];
-
-  wayland.windowManager.hyprland = {
-    enable = true;
-
-    settings.exec-once = [
-      # allow apps with risen perms after agent to connect to local xwayland
-      "${lib.getExe pkgs.xorg.xhost} +local:"
-    ];
-  };
+  wayland.windowManager.hyprland.enable = true;
 }
