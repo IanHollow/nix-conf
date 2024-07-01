24f2789d8a6c3e2a465e2e70576d26f056525dc6
Ian Holloway <ian.m.holloway@gmail.com>
2024-07-01 10:50:31 -0700

    feat: Set system version to 24.11 and enable dconf
    This commit sets the system version to 24.11

--- hosts/shared/base/base.nix
+++ hosts/shared/base/base.nix
@@ -1,5 +1,8 @@
 { pkgs, ... }:
 {
+  # set the systemVersion for the entire configuration
+  system.stateVersion = "24.11";
+
   # enable dconf
   programs.dconf.enable = true;
   environment.systemPackages = [ pkgs.gnome.dconf-editor ];
