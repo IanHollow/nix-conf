65f92cb67febef71094c34ab09770f189b31181e
Ian Holloway <ian.m.holloway@gmail.com>
2024-07-01 10:53:05 -0700

    refactor: Remove unused settings for boot and grub

--- hosts/shared/boot/grub/default.nix
+++ hosts/shared/boot/grub/default.nix
@@ -1,19 +1,14 @@
 {
   boot = {
-    initrd.systemd.strip = false;
-    initrd.systemd.enable = true;
-
     loader = {
       efi = {
         canTouchEfiVariables = true;
-        efiSysMountPoint = "/boot";
       };
 
       grub = {
         enable = true;
         efiSupport = true;
         device = "nodev";
-        configurationLimit = 10;
       };
     };
   };
