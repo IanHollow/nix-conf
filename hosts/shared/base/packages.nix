953a9d845767a3a91c0f0a8f2af29a0743c239ef
Ian Holloway <ian.m.holloway@gmail.com>
2024-07-07 21:52:28 -0700

    add base pkgs to lib

--- hosts/shared/base/packages.nix
+++ hosts/shared/base/packages.nix
@@ -12,58 +12,7 @@
 
   # Packages which are appropriate for a typical Linux system.
   # There should be **no GUI programs** in this list.
-  environment.systemPackages = with pkgs; [
-    ##################
-    ### ESSENTIALS ###
-    ##################
-
-    ### CLI UTILITIES ###
-    fastfetch # neofetch but made in c
-    wget # simple downloader utility
-    curl # network request utility
-    p7zip # archive and compression tool
-    git # version control
-    zip # archive utility
-    unzip # archive utility
-    bat # cat with wings
-    fzf # fuzzy finder
-    eza # colored alternative to ls
-    ripgrep # grep but rust
-    sd # sed but rust
-    bc
-    tree # directory tree viewer
-    rsync # remote sync
-    bind.dnsutils # dns utilities
-
-    ### CODE EDITORS ###
-    neovim
-
-    ################
-    ### HARDWARE ###
-    ################
-
-    ### SYSTEM DEVICES ###
+  environment.systemPackages = (lib.cust.basePkgs { inherit pkgs; }) ++ [
     config.boot.kernelPackages.cpupower
-    v4l-utils # proprietary media hardware and encoding
-    pciutils # utilities for pci and pcie devices
-    lshw # list hardware
-
-    ### STORAGE DEVICE DRIVERS ###
-    cryptsetup
-
-    ### STORAGE DEVICE TOOLS ###
-    gptfdisk
-    e2fsprogs
-
-    ### HARDWARE DIAGNOSTICS ###
-    smartmontools # for drive SMART status
-    btop # system process monitor
-    bottom # not top
-    procs # process viewer
-    du-dust # du but rust
-    bandwhich # network monitor
-
-    ### VIRTUALIZATION ###
-    libguestfs # filesystem driver for vm images
   ];
 }
---
+++ parts/lib/basePkgs.nix
@@ -0,0 +1,55 @@
+{ pkgs, ... }:
+(with pkgs; [
+  ##################
+  ### ESSENTIALS ###
+  ##################
+
+  ### CLI UTILITIES ###
+  fastfetch # neofetch but made in c
+  wget # simple downloader utility
+  curl # network request utility
+  p7zip # archive and compression tool
+  git # version control
+  zip # archive utility
+  unzip # archive utility
+  bat # cat with wings
+  fzf # fuzzy finder
+  eza # colored alternative to ls
+  ripgrep # grep but rust
+  sd # sed but rust
+  bc
+  tree # directory tree viewer
+  rsync # remote sync
+  bind.dnsutils # dns utilities
+  nixos-install-tools # commands from the NixOS installer
+
+  ### CODE EDITORS ###
+  neovim
+
+  ################
+  ### HARDWARE ###
+  ################
+
+  ### SYSTEM DEVICES ###
+  v4l-utils # proprietary media hardware and encoding
+  pciutils # utilities for pci and pcie devices
+  lshw # list hardware
+
+  ### STORAGE DEVICE DRIVERS ###
+  cryptsetup
+
+  ### STORAGE DEVICE TOOLS ###
+  gptfdisk
+  e2fsprogs
+
+  ### HARDWARE DIAGNOSTICS ###
+  smartmontools # for drive SMART status
+  btop # system process monitor
+  bottom # not top
+  procs # process viewer
+  du-dust # du but rust
+  bandwhich # network monitor
+
+  ### VIRTUALIZATION ###
+  libguestfs # filesystem driver for vm images
+])
