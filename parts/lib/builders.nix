cfbb2f6b36b9006a74a2d435ad38d231fda0de30
Ian Holloway <ian.m.holloway@gmail.com>
2024-07-07 21:46:58 -0700

    update home-manager integration into nixos

--- parts/lib/builders.nix
+++ parts/lib/builders.nix
@@ -63,10 +63,27 @@ let
           homeManager.nixosModules.home-manager
           # General NixOS home-manager config
           {
-            home-manager.useGlobalPkgs = true;
-            home-manager.useUserPackages = true;
-            home-manager.extraSpecialArgs = homeSpecialArgs;
-            home-manager.backupFileExtension = "backup";
+            home-manager = {
+              # tell home-manager to be as verbose as possible
+              verbose = true;
+
+              # use the system configuration’s pkgs argument
+              # this ensures parity between nixos' pkgs and hm's pkgs
+              useGlobalPkgs = true;
+
+              # enable the usage user packages through
+              # the users.users.<name>.packages option
+              useUserPackages = true;
+
+              # move existing files to the .hm.old suffix rather than failing
+              # with a very long error message about it
+              backupFileExtension = "hm.old";
+
+              # extra specialArgs passed to Home Manager
+              # for reference, the config argument in nixos can be accessed
+              # in home-manager through osConfig without us passing it
+              extraSpecialArgs = homeSpecialArgs;
+            };
           }
         ];
       in
