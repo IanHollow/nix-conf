f56a229b00f468ea00d06ca545ca343cc1870435
Ian Holloway <ian.m.holloway@gmail.com>
2024-07-07 21:43:56 -0700

    add new storage module

---
+++ hosts/shared/hardware/storage.nix
@@ -0,0 +1,25 @@
+{ config, ... }:
+{
+  services = {
+    # enable GVfs, a userspace virtual filesystem.
+    gvfs.enable = true;
+
+    # storage daemon required for udiskie auto-mount
+    udisks2 = {
+      enable = !config.boot.isContainer;
+      settings = {
+        # general udisks2 configuration
+        "udisks2.conf" = {
+          defaults = {
+            encryption = "luks2"; # alternatively, luks1
+          };
+
+          udisks2 = {
+            modules = [ "*" ];
+            modules_load_preference = "ondemand";
+          };
+        };
+      };
+    };
+  };
+}
