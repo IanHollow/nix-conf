fb77b4711523afadc77237f6fbcc6468d9b347c1
Ian Holloway <ian.m.holloway@gmail.com>
2024-07-07 21:45:41 -0700

    add more security features

--- hosts/shared/security/polkit.nix
+++ hosts/shared/security/polkit.nix
@@ -1,4 +1,9 @@
-{ config, lib, ... }:
+{
+  config,
+  pkgs,
+  lib,
+  ...
+}:
 {
   security.polkit = {
     enable = true;
@@ -12,5 +17,21 @@
         polkit.log("user " +  subject.user + " is attempting action " + action.id + " from PID " + subject.pid);
       });
     '';
+  };
+
+  systemd = {
+    user.services.polkit-gnome-authentication-agent-1 = {
+      description = "polkit-gnome-authentication-agent-1";
+      wantedBy = [ "graphical-session.target" ];
+      wants = [ "graphical-session.target" ];
+      after = [ "graphical-session.target" ];
+      serviceConfig = {
+        Type = "simple";
+        ExecStart = "${pkgs.polkit_gnome}/libexec/polkit-gnome-authentication-agent-1";
+        Restart = "on-failure";
+        RestartSec = 1;
+        TimeoutStopSec = 10;
+      };
+    };
   };
 }
