c661bc36da178fb45c67a3915a9b90f4101b8458
Ian Holloway <ian.m.holloway@gmail.com>
2024-08-05 13:52:42 -0700

    add basic neovim config

---
+++ configs/home/programs/editors/neovim/default.nix
@@ -0,0 +1,25 @@
+{ inputs, pkgs, ... }:
+{
+  imports = [
+    inputs.nixvim.homeManagerModules.nixvim
+
+    ./config
+  ];
+
+  programs.nixvim = {
+    enable = true;
+
+    # Aliases
+    viAlias = true;
+    vimAlias = true;
+
+    # Clipboard
+    clipboard = {
+      # Enable clipboard support
+      providers.wl-copy.enable = true;
+
+      # Allow copying to the system clipboard
+      register = "unnamedplus";
+    };
+  };
+}
