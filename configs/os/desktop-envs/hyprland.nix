cff3124b0fc3373e0286e810c2faab08e9909032
Ian Holloway <ian.m.holloway@gmail.com>
2024-08-05 14:03:49 -0700

    update hyprland be simpler for nixos

--- configs/os/desktop-envs/hyprland.nix
+++ configs/os/desktop-envs/hyprland.nix
@@ -1,19 +1,8 @@
-{ pkgs, inputs, ... }:
+{ pkgs, ... }:
 {
-  imports = [ inputs.hyprland.nixosModules.default ];
   programs.hyprland.enable = true;
 
-  hardware.graphics =
-    let
-      pkgs-unstable = inputs.hyprland.inputs.nixpkgs.legacyPackages.${pkgs.stdenv.hostPlatform.system};
-    in
-    {
-      package = pkgs-unstable.mesa.drivers;
-
-      # if you also want 32-bit support (e.g for Steam)
-
-      package32 = pkgs-unstable.pkgsi686Linux.mesa.drivers;
-    };
+  environment.systemPackages = with pkgs; [ xwaylandvideobridge ];
 
   programs.xwayland.enable = true;
 }
