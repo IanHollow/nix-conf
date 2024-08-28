70aecf4be2fb7fbffd87ba2300ecb6bf814c1476
Ian Holloway <ian.m.holloway@gmail.com>
2024-08-28 09:55:49 -0700

    add cosmic desktop

---
+++ configs/os/desktop-envs/cosmic.nix
@@ -0,0 +1,8 @@
+{ inputs, ... }:
+{
+  imports = [ inputs.cosmic.nixosModules.default ];
+
+  services.displayManager.cosmic-greeter.enable = true;
+
+  services.desktopManager.cosmic.enable = true;
+}
