ce9b2ac64c7bad6ae97e5b2a4b1a4ea93d202a5c
Ian Holloway <ian.m.holloway@gmail.com>
2024-08-28 10:03:57 -0700

    mount a drive to user all users home dir

--- hosts/desktop/hardware/filesystems.nix
+++ hosts/desktop/hardware/filesystems.nix
@@ -15,6 +15,21 @@ let
   mkBTRFS = label: subvol: (mkFS label "btrfs") // btrfsOptions subvol;
   mkBoot = label: mkFS label "vfat";
   bootMP = config.boot.loader.efi.efiSysMountPoint;
+
+  # Mount a drive to user's home directory for all normal users
+  # use custom option that stores all normal users
+  users = config.users.normalUsers;
+  # map each user to a name value pair for the mount point
+  homeMounts =
+    name: mount:
+    lib.pipe users [
+      (builtins.map (user: (lib.attrsets.nameValuePair "${user.home}/${name}" mount)))
+      builtins.listToAttrs
+    ];
+
+  # Define the games drive mount points
+  gamesDrive = homeMounts "games" (mkBTRFS "games" "games");
+
 in
 {
   fileSystems = {
@@ -25,7 +40,7 @@ in
     "/home" = mkBTRFS rootLabel "@home";
 
     ${bootMP} = mkBoot bootLabel;
-  };
+  } // gamesDrive;
 
   # Swap
   swapDevices = [ { label = swapLabel; } ];
