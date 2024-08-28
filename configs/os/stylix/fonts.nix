51565ff24f4bbdd341f33647c3b93013934ffd6b
Ian Holloway <ian.m.holloway@gmail.com>
2024-08-28 09:57:49 -0700

    change fontconfig

--- configs/os/stylix/fonts.nix
+++ configs/os/stylix/fonts.nix
@@ -1,4 +1,4 @@
-{ pkgs, ... }:
+{ pkgs, config, ... }:
 {
   stylix.fonts = {
     monospace = {
@@ -19,6 +19,41 @@
     emoji = {
       package = pkgs.noto-fonts-emoji;
       name = "Noto Color Emoji";
+    };
+  };
+
+  fonts.fontconfig = {
+    enable = true;
+    defaultFonts = {
+      serif = [
+        config.stylix.fonts.serif.name
+        "Noto Serif CJK SC"
+        "Noto Serif CJK TC"
+        "Noto Serif CJK HK"
+        "Noto Serif CJK JP"
+        "Noto Serif CJK KR"
+        "Noto Color Emoji"
+      ];
+      sansSerif = [
+        config.stylix.fonts.sansSerif.name
+        "Noto Sans CJK SC"
+        "Noto Sans CJK TC"
+        "Noto Sans CJK HK"
+        "Noto Sans CJK JP"
+        "Noto Sans CJK KR"
+      ];
+      monospace = [
+        config.stylix.fonts.monospace.name
+        "Noto Sans Mono CJK SC"
+        "Noto Sans Mono CJK TC"
+        "Noto Sans Mono CJK HK"
+        "Noto Sans Mono CJK JP"
+        "Noto Sans Mono CJK KR"
+      ];
+      emoji = [
+        config.stylix.fonts.emoji.name
+        "Noto Color Emoji"
+      ];
     };
   };
 }
