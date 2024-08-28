4d2175e3c9d568c5c0cdddd09446d40efd00947e
Ian Holloway <ian.m.holloway@gmail.com>
2024-08-28 16:22:01 -0700

    update wine

--- configs/home/gaming/wine.nix
+++ configs/home/gaming/wine.nix
@@ -1,7 +1,17 @@
-{ pkgs, config, ... }:
+{
+  pkgs,
+  config,
+  inputs,
+  ...
+}:
 {
   home.packages = [
-    (pkgs.wineWowPackages.stagingFull.override { waylandSupport = true; })
+    # (pkgs.wineWowPackages.unstableFull.override { waylandSupport = true; })
+    (inputs.nix-gaming.packages.${pkgs.system}.wine-tkg.override (old: {
+      supportFlags = old.supportFlags // {
+        waylandSupport = true; # Causes a cache miss so a build is needed
+      };
+    }))
     pkgs.winetricks
   ];
 
