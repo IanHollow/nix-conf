c4ee66b880663d3454557841f66f376997fa60b6
Ian Holloway <ian.m.holloway@gmail.com>
2024-08-28 10:00:16 -0700

    add more wayland vars

--- parts/lib/env/wayland/base.nix
+++ parts/lib/env/wayland/base.nix
@@ -1,4 +1,9 @@
 {
   XDG_SESSION_TYPE = "wayland";
   NIXOS_OZONE_WL = "1";
+
+  GDK_BACKEND = "wayland";
+
+  SDL_VIDEODRIVER = "wayland,x11,windows"; # add fallbacks so that easyanticheat works
+  CLUTTER_BACKEND = "wayland";
 }
