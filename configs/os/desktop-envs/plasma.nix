b8cb2770ef401d0d2a5bd93bb571afc348a6301a
Ian Holloway <ian.m.holloway@gmail.com>
2024-08-28 09:56:05 -0700

    change plasma config

--- configs/os/desktop-envs/plasma.nix
+++ configs/os/desktop-envs/plasma.nix
@@ -1,11 +1,1 @@
-{
-  services = {
-    displayManager.sddm = {
-      enable = true;
-      wayland.enable = true;
-      wayland.compositor = "kwin";
-    };
-
-    desktopManager.plasma6.enable = true;
-  };
-}
+{ services.desktopManager.plasma6.enable = true; }
