c666fd3fd22539f13e337c52b27969768e909f98
Ian Holloway <ian.m.holloway@gmail.com>
2024-09-01 16:54:02 -0700

    update flake

--- flake.nix
+++ flake.nix
@@ -173,8 +173,11 @@
     nix-gaming = {
       url = "github:fufexan/nix-gaming";
       inputs = {
-        nixpkgs.follows = "nixpkgs";
         flake-parts.follows = "flake-parts";
+
+        umu.follows = "";
+
+        # Don't overwrite nixpkgs as this could cause cache miss
       };
     };
 
@@ -206,13 +209,24 @@
       };
     };
 
+    # Provide NixGL for Geospatial Nix
+    nixgl = {
+      url = "github:nix-community/nixGL";
+      inputs = {
+        nixpkgs.follows = "nixpkgs";
+        flake-utils.follows = "flake-utils";
+      };
+    };
+
     # Geospatial Nix
     geospatial-nix = {
       url = "github:imincik/geospatial-nix";
       inputs = {
         flake-compat.follows = "flake-compat";
         flake-utils.follows = "flake-utils";
-        # Don't import nixpkgs as this could cause cache miss
+        nixgl.follows = "nixgl";
+
+        # Don't overwrite nixpkgs as this could cause cache miss
       };
     };
 
@@ -235,12 +249,24 @@
       };
     };
 
+    # Provide nix-update for cosmic
+    nix-update = {
+      url = "github:Mic92/nix-update";
+      inputs = {
+        nixpkgs.follows = "nixpkgs";
+        flake-parts.follows = "flake-parts";
+        treefmt-nix.follows = "treefmt-nix";
+      };
+    };
+
     # Cosmic Desktop
     cosmic = {
       url = "github:lilyinstarlight/nixos-cosmic";
       inputs = {
-        nixpkgs.follows = "nixpkgs";
         flake-compat.follows = "flake-compat";
+        nix-update.follows = "nix-update";
+
+        # Don't overwrite nixpkgs as this could cause cache miss
       };
     };
   };
@@ -249,7 +275,6 @@
     extra-substituters = [
       "https://cache.nixos.org" # official nix cache
       "https://nix-community.cachix.org" # nix-community cache
-      "https://hyprland.cachix.org" # hyprland
       "https://nixpkgs-unfree.cachix.org" # unfree-package cache
       "https://cache.garnix.io" # garnix binary cache
       "https://geonix.cachix.org" # geospatial nix
@@ -260,7 +285,6 @@
     extra-trusted-public-keys = [
       "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
-      "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
       "nixpkgs-unfree.cachix.org-1:hqvoInulhbV4nJ9yJOEr+4wxhDV4xq2d1DK7S6Nj6rs="
       "cache.garnix.io:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g="
       "geonix.cachix.org-1:iyhIXkDLYLXbMhL3X3qOLBtRF8HEyAbhPXjjPeYsCl0="
