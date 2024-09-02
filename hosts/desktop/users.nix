8d5dbb91c7bd019fe218d8132757cda08fb6bc99
Ian Holloway <ian.m.holloway@gmail.com>
2024-09-01 20:03:16 -0700

    update addUser and simplify user config

--- hosts/desktop/users.nix
+++ hosts/desktop/users.nix
@@ -1,52 +1,39 @@
 {
+  config,
   lib,
   tree,
-  pkgs,
-  inputs,
   ...
 }:
-{
-  imports =
-    with lib.cust.nixos;
-    let
-      homes =
-        name:
-        tree.homes.${name}.modules {
-          inherit
-            tree
-            lib
-            inputs
-            pkgs
-            ;
-        };
-    in
-    [
-      (addUser {
-        username = "ianmh";
-        description = "Ian Holloway";
-        extraGroups = [
-          "wheel"
-          "audio"
-          "video"
-          "input"
-          "docker"
-          "libvirtd"
-          "network"
-          "networkmanager"
-        ];
-        initialPassword = "password";
-        homeManagerModules = homes "desktop";
-      })
-
-      # (addUser {
-      #   username = "guest";
-      #   description = "Guest User";
-      #   extraGroups = [
-      #     "audio"
-      #     "video"
-      #   ];
-      #   initialPassword = "password";
-      #   homeManagerModules = homeConfigs.desktop.modules { inherit tree; };
-      # })
+let
+  addUser = lib.cust.nixos.addUser { inherit config lib; };
+  homes = name: tree.homes.${name}.modules;
+in
+lib.mkMerge [
+  (addUser {
+    username = "ianmh";
+    description = "Ian Holloway";
+    extraGroups = [
+      "wheel"
+      "audio"
+      "video"
+      "input"
+      "docker"
+      "libvirtd"
+      "network"
+      "networkmanager"
     ];
-}
+    initialPassword = "password";
+    homeManagerModules = homes "desktop";
+  })
+
+  # (addUser {
+  #   username = "guest";
+  #   description = "Guest User";
+  #   extraGroups = [
+  #     "audio"
+  #     "video"
+  #   ];
+  #   initialPassword = "password";
+  #   homeManagerModules = homeConfigs.desktop.modules { inherit tree; };
+  # })
+]
