dd7cb8ec5dbc74c0e6a21b8d1d908713c0d88e81
Ian Holloway <ian.m.holloway@gmail.com>
2024-09-01 21:09:04 -0700

    remove old code that is redundant

--- hosts/desktop/default.nix
+++ hosts/desktop/default.nix
@@ -1,7 +1,6 @@
 {
   tree,
   hostname,
-  lib,
   inputs,
   ...
 }:
@@ -11,88 +10,84 @@
     config.allowUnfree = true;
   };
 
-  # TODO: rewrite map function to import everything if no default and is dir
-  modules = builtins.map (x: if x ? "default" then x.default else x) (
-    with (tree.hosts.${hostname} // tree.hosts.${hostname}.hardware // tree.configs.os);
-    [
-      ## Base
-      base.nix-settings
-      base.nix-registry
-      base.base
-      base.kernel
-      base.packages
-      users
+  modules = with (tree.hosts.${hostname} // tree.hosts.${hostname}.hardware // tree.configs.os); [
+    ## Base
+    base.nix-settings
+    base.nix-registry
+    base.base
+    base.kernel
+    base.packages
+    users
 
-      ## Boot
-      boot.generic
-      boot.grub
-      boot.grub.dual-boot
-      kernel-modules
-      # plymouth
+    ## Boot
+    boot.generic
+    boot.grub
+    boot.grub.dual-boot
+    kernel-modules
+    # plymouth
 
-      ## Locale
-      timezone
-      locale.timesync
-      locale.fonts
-      # keyboard
+    ## Locale
+    timezone
+    locale.timesync
+    locale.fonts
+    # keyboard
 
-      ## Hardware
-      filesystems
-      gpu
-      cpu
-      # kernel-patches
-      power
-      audio
-      monitor
-      hardware.zram
-      hardware.networking
-      hardware.bluetooth
-      hardware.tpm
-      hardware.firmware
-      hardware.sound
-      hardware.ssd
-      hardware.storage
-      peripherals.mouse
-      gaming
+    ## Hardware
+    filesystems
+    gpu
+    cpu
+    # kernel-patches
+    power
+    audio
+    monitor
+    hardware.zram
+    hardware.networking
+    hardware.bluetooth
+    hardware.tpm
+    hardware.firmware
+    hardware.sound
+    hardware.ssd
+    hardware.storage
+    peripherals.mouse
+    gaming
 
-      ## Virtualization
-      virtualisation.docker
-      virtualisation.libvirt
+    ## Virtualization
+    virtualisation.docker
+    virtualisation.libvirt
 
-      ## Desktop Environments
-      # desktop-envs.gnome
-      # desktop-envs.plasma
-      desktop-envs.hyprland
-      # desktop-envs.cosmic
+    ## Desktop Environments
+    # desktop-envs.gnome
+    # desktop-envs.plasma
+    desktop-envs.hyprland
+    # desktop-envs.cosmic
 
-      ## Display Managers
-      # display-managers.greetd
-      display-managers.gdm
-      # display-managers.sddm
+    ## Display Managers
+    # display-managers.greetd
+    display-managers.gdm
+    # display-managers.sddm
 
-      ## Security
-      security
+    ## Security
+    security
 
-      ## Services
-      services.runners
-      services.disable-hibernate
-      services.earlyoom
+    ## Services
+    services.runners
+    services.disable-hibernate
+    services.earlyoom
 
-      # Programs
-      # TODO: add neovim base config and better config in home-manager
+    # Programs
+    # TODO: add neovim base config and better config in home-manager
 
-      ## Theming
-      stylix.base
-      stylix.cursor
-      stylix.fonts
+    ## Theming
+    stylix.base
+    stylix.cursor
+    stylix.fonts
 
-      ## Server
-      # server.minecraft
+    ## Server
+    # server.minecraft
 
-      ## Environment Variables
-      { environment.sessionVariables = tree.configs.shared.env.wayland.default; }
-    ]
-  );
+    ## Environment Variables
+    { environment.sessionVariables = tree.configs.shared.env.wayland.default; }
+  ];
 
   overlays = [
     inputs.nur.overlay
