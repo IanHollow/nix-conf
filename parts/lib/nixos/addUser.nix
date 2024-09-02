eb4eab4365e56e71d9788193c389d0ef6eb38081
Ian Holloway <ian.m.holloway@gmail.com>
2024-09-01 20:03:16 -0700

    update addUser and simplify user config

--- parts/lib/nixos/addUser.nix
+++ parts/lib/nixos/addUser.nix
@@ -1,3 +1,4 @@
+{ config, lib, ... }:
 {
   username,
   homeDirectory ? "/home/${username}",
@@ -7,24 +8,8 @@
   isNormalUser ? true,
   homeManagerModules ? [ ],
 }:
-{ config, lib, ... }:
-{
-  config = {
-    assertions = [
-      {
-        assertion = config.users ? normalUsers;
-        message = "Import custom users module to use normalUsers list";
-      }
-    ];
-
-    # Add user to custom normalUsers list
-    users.normalUsers = [
-      {
-        inherit username;
-        home = homeDirectory;
-      }
-    ];
-
+lib.mkMerge [
+  {
     users.users.${username} = {
       inherit
         description
@@ -34,29 +19,57 @@
         ;
       home = lib.mkForce homeDirectory;
     };
-    home-manager.users.${username} = {
-      imports = homeManagerModules;
+  }
 
-      # Use the same nix package as nixos
-      nix.package = lib.mkForce config.nix.package;
+  (lib.mkIf (config.users ? normalUsers) {
+    # Add user to custom normalUsers list
+    users.normalUsers = [
+      {
+        inherit username;
+        home = homeDirectory;
+      }
+    ];
+  })
 
-      # Allow HM to manage itself when in standalone mode.
-      # This makes the home-manager command available to users.
-      programs.home-manager.enable = true;
+  (lib.mkIf (config ? home-manager) {
+    home-manager.users.${username} =
+      {
+        tree,
+        lib,
+        pkgs,
+        inputs,
+        ...
+      }:
+      {
+        imports = homeManagerModules {
+          inherit
+            tree
+            lib
+            pkgs
+            inputs
+            ;
+        };
 
-      # Disable home-manager man-pages to save space
-      manual = {
-        manpages.enable = false;
-        html.enable = false;
-        json.enable = false;
+        # Use the same nix package as nixos
+        nix.package = lib.mkForce config.nix.package;
+
+        # Allow HM to manage itself when in standalone mode.
+        # This makes the home-manager command available to users.
+        programs.home-manager.enable = true;
+
+        # Disable home-manager man-pages to save space
+        manual = {
+          manpages.enable = false;
+          html.enable = false;
+          json.enable = false;
+        };
+
+        # Set default settings based on the nixos settings
+        home = {
+          username = lib.mkForce username;
+          stateVersion = lib.mkForce config.system.stateVersion;
+          homeDirectory = lib.mkForce homeDirectory;
+        };
       };
-
-      # Set default settings based on the nixos settings
-      home = {
-        username = lib.mkForce username;
-        stateVersion = lib.mkForce config.system.stateVersion;
-        homeDirectory = lib.mkForce homeDirectory;
-      };
-    };
-  };
-}
+  })
+]
