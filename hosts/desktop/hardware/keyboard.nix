64f55b0a7bec18f00e231a60dcf8f80c0bba1b31
Ian Holloway <ian.m.holloway@gmail.com>
2024-10-20 22:00:09 -0700

    enable wooting software

---
+++ hosts/desktop/hardware/keyboard.nix
@@ -0,0 +1,12 @@
+{ pkgs, self, ... }:
+{
+  # Wooting Keyboard
+  # hardware.wooting.enable = true;
+
+  environment.systemPackages = [
+    pkgs.wootility
+  ];
+  services.udev.packages = [
+    self.packages.${pkgs.system}.wooting-udev-rules-80he
+  ];
+}
