897d6fe778e8c27c01fc3697d90a233da1ba77b1
Ian Holloway <ian.m.holloway@gmail.com>
2024-11-01 22:30:50 -0700

    add window rules for Wine and Xwayland Video Bridge in Hyprland configuration

--- configs/os/desktop-envs/hyprland.nix
+++ configs/os/desktop-envs/hyprland.nix
@@ -5,23 +5,22 @@
     # set the flake package
     package = inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.hyprland;
     # make sure to also set the portal package, so that they are in sync
-    portalPackage =
-      inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.xdg-desktop-portal-hyprland;
+    portalPackage = inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.xdg-desktop-portal-hyprland;
   };
 
   security.pam.services.hyprlock = { };
 
   hardware.graphics =
-    let
-      nixpkgs-hyprland = inputs.hyprland.inputs.nixpkgs.legacyPackages.${pkgs.stdenv.hostPlatform.system};
-    in
-    {
-      enable = true;
-      extraPackages = [ nixpkgs-hyprland.mesa.drivers ];
-
-      enable32Bit = true;
-      extraPackages32 = [ nixpkgs-hyprland.pkgsi686Linux.mesa.drivers ];
-    };
+  let
+    nixpkgs-hyprland = inputs.hyprland.inputs.nixpkgs.legacyPackages.${pkgs.stdenv.hostPlatform.system};
+  in
+  {
+    enable = true;
+    extraPackages = [ nixpkgs-hyprland.mesa.drivers ];
+    
+    enable32Bit = true;
+    extraPackages32 = [ nixpkgs-hyprland.pkgsi686Linux.mesa.drivers ];
+  };
 
   environment.systemPackages = with pkgs; [ xwaylandvideobridge ];
 
