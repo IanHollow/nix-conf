461a2b7f02f8abdb17aad5ea72dfdc2cc1d17882
Ian Holloway <ian.m.holloway@gmail.com>
2024-11-20 11:31:47 -0800

    update nix state version 25.05

--- configs/os/base/base.nix
+++ configs/os/base/base.nix
@@ -1,7 +1,7 @@
 { pkgs, ... }:
 {
   # set the systemVersion for the entire configuration
-  system.stateVersion = "24.11";
+  system.stateVersion = "25.05";
 
   # enable dconf
   programs.dconf.enable = true;
