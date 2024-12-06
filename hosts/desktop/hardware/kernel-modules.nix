dcb1f275ff6bc25863fbcd26770f87fdb1867ae3
Ian Holloway <ian.m.holloway@gmail.com>
2024-12-06 12:17:25 -0800

    add default kernel modules to initrd configuration for desktop and laptop

--- hosts/desktop/hardware/kernel-modules.nix
+++ hosts/desktop/hardware/kernel-modules.nix
@@ -1,10 +1,11 @@
+{ lib, ... }:
 {
+  # Set of base kernel modules to include in the initrd
+  # Found here: https://github.com/NixOS/nixpkgs/blob/nixos-unstable/nixos/modules/system/boot/kernel.nix
+  boot.initrd.includeDefaultModules = lib.mkForce true;
+
   boot.initrd.availableKernelModules = [
-    "nvme"
-    "ahci"
-    "xhci_pci"
-    "usbhid"
-    "usb_storage"
-    "sd_mod"
+    "usb_storage" # USB storage devices
+    "uas" # USB storage devices
   ];
 }
--- hosts/laptop/hardware/kernel-modules.nix
+++ hosts/laptop/hardware/kernel-modules.nix
@@ -1,10 +1,13 @@
+{ lib, ... }:
 {
+  # Set of base kernel modules to include in the initrd
+  # Found here: https://github.com/NixOS/nixpkgs/blob/nixos-unstable/nixos/modules/system/boot/kernel.nix
+  boot.initrd.includeDefaultModules = lib.mkForce true;
+
   boot.initrd.availableKernelModules = [
-    "nvme"
-    "ahci"
-    "xhci_pci"
-    "usbhid"
-    "usb_storage"
-    "sd_mod"
+    "usb_storage" # USB storage devices
+    "uas" # USB storage devices
+    "rtsx_pci_sdmmc" # Realtek PCI-E SD/MMC Card Host Driver
+    "snd_hda_intel" # Intel HD Audio
   ];
 }
