59a7310741e93d464e0ef126e574a90b4e45e4b3
Ian Holloway <ian.m.holloway@gmail.com>
2024-12-06 11:51:56 -0800

    refactor btrfs options to allow additional parameters and set default options

--- hosts/desktop/hardware/filesystems.nix
+++ hosts/desktop/hardware/filesystems.nix
@@ -5,14 +5,18 @@ let
   bootLabel = "boot";
 
   mkFS = label: fsType: { inherit label fsType; };
-  btrfsOptions = subvol: {
+  btrfsOptions = subvol: extra: {
     options = [
       "subvol=${subvol}"
-      "compress-force=zstd:1"
-      "noatime"
-    ];
+    ] ++ extra;
   };
-  mkBTRFS = label: subvol: (mkFS label "btrfs") // btrfsOptions subvol;
+  defaultBTRFSOptions = [
+    "compress-force=zstd:1"
+    "noatime"
+  ];
+  mkBTRFS =
+    label: subvol: extra:
+    (mkFS label "btrfs") // (btrfsOptions subvol extra);
   mkBoot = label: mkFS label "vfat";
   bootMP = config.boot.loader.efi.efiSysMountPoint;
 
@@ -28,18 +32,21 @@ let
     ];
 
   # Define the games drive mount points
-  gamesDrive = homeMounts "games" (mkBTRFS "games" "games");
+  gamesDrive = homeMounts "games" (mkBTRFS "games" "games" defaultBTRFSOptions);
 
 in
 {
   fileSystems = {
-    "/" = mkBTRFS rootLabel "@root";
-    "/var" = mkBTRFS rootLabel "@var";
-    "/tmp" = mkBTRFS rootLabel "@tmp";
-    "/nix" = mkBTRFS rootLabel "@nix";
-    "/home" = mkBTRFS rootLabel "@home";
+    "/" = mkBTRFS rootLabel "@root" defaultBTRFSOptions;
+    "/var" = mkBTRFS rootLabel "@var" defaultBTRFSOptions;
+    "/tmp" = mkBTRFS rootLabel "@tmp" defaultBTRFSOptions;
+    "/nix" = mkBTRFS rootLabel "@nix" defaultBTRFSOptions;
+    "/home" = mkBTRFS rootLabel "@home" [
+      "compress-force=zstd:1"
+      "relatime"
+    ];
 
-    ${bootMP} = mkBoot bootLabel;
+    ${bootMP} = mkBoot bootLabel; # should be /boot by default
   } // gamesDrive;
 
   # Swap
