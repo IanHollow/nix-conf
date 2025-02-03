f9b7958f841da7f5c2a7571dab9ceafb3e41a01f
Ian Holloway <ian.m.holloway@gmail.com>
2025-02-03 14:39:20 -0800

    add packages to ags

--- configs/home/programs/ags/default.nix
+++ configs/home/programs/ags/default.nix
@@ -1,7 +1,13 @@
-{ inputs, pkgs, ... }:
+{
+  inputs,
+  pkgs,
+  ...
+}:
 {
   # add the home manager module
   imports = [ inputs.ags.homeManagerModules.default ];
+
+  home.packages = [ inputs.astal.packages.${pkgs.system}.default ];
 
   programs.ags = {
     enable = true;
@@ -10,9 +16,19 @@
     configDir = ../ags;
 
     # additional packages to add to gjs's runtime
-    extraPackages = with pkgs; [
-      inputs.ags.packages.${pkgs.system}.battery
-      fzf
+    extraPackages = with inputs.astal.packages.${pkgs.system}; [
+      astal4
+
+      io
+      apps
+      battery
+      bluetooth
+      hyprland
+      mpris
+      network
+      notifd
+      tray
+      wireplumber
     ];
   };
 }
