0012213bbdedec339d4268ba52093732c3947f17
Ian Holloway <ian.m.holloway@gmail.com>
2025-03-29 18:32:18 -0700

    remove old wip agenix config

--- homes/laptop/secrets/default.nix
+++ homes/laptop/secrets/default.nix
@@ -1,9 +1,3 @@
-{ inputs, pkgs, ... }:
-# TODO: check if this already configured in NixOS
+{ ... }:
 {
-  imports = [ inputs.agenix.homeManagerModules.default ];
-
-  home.packages = [
-    inputs.agenix.packages.${pkgs.system}.agenix
-  ];
 }
