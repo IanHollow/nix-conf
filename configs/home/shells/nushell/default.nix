4b90c1ae2d761b970fc6318af5d79b4d368a7998
Ian Holloway <ian.m.holloway@gmail.com>
2025-04-24 11:17:00 -0700

    refactor: update nushell config to include home-manager settings

--- configs/home/shells/nushell/default.nix
+++ configs/home/shells/nushell/default.nix
@@ -1,7 +1,12 @@
-{ ... }:
+{ config, ... }:
 {
   programs.nushell = {
     enable = true;
+
+    # Move home-manager settings to the nushell config
+    # https://github.com/nix-community/home-manager/issues/4313
+    shellAliases = config.home.shellAliases;
+    environmentVariables = config.home.sessionVariables;
 
     settings = {
       # Remove the welcome banner message
