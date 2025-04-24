7991f822c97d7168e0631449549e9e3c0e3a8a48
Ian Holloway <ian.m.holloway@gmail.com>
2025-04-24 14:17:49 -0700

    fix: update nushell integration condition to use dynamic shell package name

--- parts/lib/darwin/addUser.nix
+++ parts/lib/darwin/addUser.nix
@@ -163,7 +163,7 @@ lib.mkMerge [
                   lib.mapAttrsToList (
                     shellName: shellPkg:
                     lib.nameValuePair "enable${capitalize shellName}Integration" (
-                      lib.mkIf (shellPkg.pname == "nushell") true
+                      lib.mkIf (shellPkg.pname == shell.pname) true
                     )
                   ) shells
                 );
