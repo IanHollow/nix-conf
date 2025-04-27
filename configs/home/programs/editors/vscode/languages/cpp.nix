7770900587a1b48ee724f1f46e93a7e26ff3cfa8
Ian Holloway <ian.m.holloway@gmail.com>
2025-04-26 17:07:14 -0700

    remove: unused settings from cmake vscode

--- configs/home/programs/editors/vscode/languages/cpp.nix
+++ configs/home/programs/editors/vscode/languages/cpp.nix
@@ -28,7 +28,6 @@
       "editor.defaultFormatter" = "llvm-vs-code-extensions.vscode-clangd";
     };
 
-    "cmake.showOptionsMovedNotification" = false;
     "cmake.cmakePath" = lib.getExe pkgs.cmake;
     "C_Cpp.intelliSenseEngine" = "disabled"; # IntelliSense from Microsoft conflicts with clangd
     "clangd.path" = lib.getExe' pkgs.clang-tools "clangd";
