24ba96c2a1aa9380698cbc75a9db19b35f14c418
Ian Holloway <ian.m.holloway@gmail.com>
2025-05-01 15:32:03 -0700

    feat: add podman configuration for Linux and Darwin environments

---
+++ configs/home/dev/podman.nix
@@ -0,0 +1,21 @@
+{ pkgs, lib, ... }:
+let
+  linux = lib.mkIf (pkgs.stdenv.isLinux) {
+    services.podman = {
+      enable = true;
+      enableTypeChecks = true;
+
+      autoUpdate.enable = true;
+      autoUpdate.onCalendar = "Mon *-*-* 02:30";
+    };
+  };
+
+  darwin = lib.mkIf (pkgs.stdenv.isDarwin) {
+    home.packages = [
+      pkgs.podman
+    ];
+  };
+in
+lib.mkMerge [darwin linux]
+
+
