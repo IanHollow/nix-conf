77f256ed5560fdb562edefa3aa0d17465aa04940
Ian Holloway <ian.m.holloway@gmail.com>
2025-05-13 20:39:25 -0700

    feat: add MATLAB language support to VSCode configuration

---
+++ configs/home/programs/editors/vscode/languages/matlab.nix
@@ -0,0 +1,20 @@
+{
+  lib,
+  pkgs,
+  inputs,
+  ...
+}:
+{
+  programs.vscode.profiles.default.extensions =
+    let
+      extensions = pkgs.callPackage ../marketplace.nix { inherit inputs; };
+    in
+    with extensions.preferNixpkgsThenPreRelease;
+    [
+      mathworks.language-matlab
+      affenwiesel.matlab-formatter
+    ];
+
+  programs.vscode.profiles.default.userSettings = {
+  };
+}
