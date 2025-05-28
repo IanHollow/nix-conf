c43626decc17189c98cbeec58aa812062a4bdece
Ian Holloway <ian.m.holloway@gmail.com>
2025-05-28 13:12:51 -0700

    feat: add comment to clarify Apple Watch login configuration in pam.nix

--- configs/darwin/security/pam.nix
+++ configs/darwin/security/pam.nix
@@ -7,7 +7,7 @@
 
       # Sudo through Touch ID and Apple Watch
       touchIdAuth = true;
-      watchIdAuth = true;
+      watchIdAuth = true; # Allow Login with Apple Watch (need to manually enable in System Settings)
       reattach = true; # This fixes Touch ID for sudo not working inside tmux and screen.
     };
   };
