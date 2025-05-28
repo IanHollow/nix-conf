7b06bfaf136929f7092ad46323fc32b6819646de
Ian Holloway <ian.m.holloway@gmail.com>
2025-05-28 13:13:09 -0700

    feat: restructure home configuration by removing deprecated files and adding new xdg and version configurations

--- configs/home/base/misc.nix
+++
@@ -1,9 +0,0 @@
-{
-  # TODO: this is workaround and is not ideal this should ideally should not be here
-  dconf.settings = {
-    "org/virt-manager/virt-manager/connections" = {
-      autoconnect = [ "qemu:///system" ];
-      uris = [ "qemu:///system" ];
-    };
-  };
-}
