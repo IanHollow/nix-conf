18ec2ae11a407d393ff53562eeed5bb064cfefaf
Ian Holloway <ian.m.holloway@gmail.com>
2025-06-03 17:09:20 -0700

    feat: add Nushell configuration files for enhanced environment setup and script handling

--- configs/home/shells/nushell/env.nix
+++ configs/home/shells/nushell/env.nix
@@ -1,96 +1,56 @@
-{ lib, config, ... }@args:
-{
-  programs.nushell.extraEnv =
-    let
-      exportToNuEnv =
-        vars:
-        lib.concatStringsSep "\n" (
-          lib.mapAttrsToList (
-            n: v:
+{ lib, config, ... }:
+let
+  exportToNuEnv =
+    vars:
+    lib.concatStringsSep "\n" (
+      lib.mapAttrsToList (
+        n: v:
+        let
+          replaceVars =
+            varsIn: varsOut: v:
+            "$env.${n} = ${
+              if lib.typeOf v == "string" then
+                "\"${builtins.replaceStrings varsIn varsOut v}\""
+              else
+                builtins.toString v
+            }";
+          replaceVarPresets =
+            v:
             let
-              replaceVars =
-                varsIn: varsOut: v:
-                "$env.${n} = ${
-                  if lib.typeOf v == "string" then
-                    "\"${builtins.replaceStrings varsIn varsOut v}\""
-                  else
-                    builtins.toString v
-                }";
-              replaceVarPresets =
-                v:
-                let
-                  rightSideVarPreset = lib.concatStrings [
-                    "$"
-                    "{"
-                    n
-                    ":+:$"
-                    n
-                    "}"
-                    "\""
-                  ];
-                in
-                builtins.replaceStrings
-                  [
-                    rightSideVarPreset
-                  ]
-                  [
-                    ''" + (do { let x = ($env.${n}? | default ""); if $x == "" { "" } else { ":" + $x } }) | split row (char esep) | uniq''
-                  ]
-                  v;
+              rightSideVarPreset = lib.concatStrings [
+                "$"
+                "{"
+                n
+                ":+:$"
+                n
+                "}"
+                "\""
+              ];
             in
-            lib.pipe v [
-              (replaceVars
-                [
-                  "$HOME"
-                  "$USER"
-                ]
-                [ config.home.username config.home.homeDirectory ]
-              )
-              replaceVarPresets
+            builtins.replaceStrings
+              [
+                rightSideVarPreset
+              ]
+              [
+                ''" + (do { let x = ($env.${n}? | default ""); if $x == "" { "" } else { ":" + $x } }) | split row (char esep) | uniq''
+              ]
+              v;
+        in
+        lib.pipe v [
+          (replaceVars
+            [
+              "$HOME"
+              "$USER"
             ]
-          ) vars
-        );
-
-      paths =
-        [
-          config.home.profileDirectory
+            [ config.home.username config.home.homeDirectory ]
+          )
+          replaceVarPresets
         ]
-        ++ lib.optionals (args ? darwinConfig) args.darwinConfig.environment.profiles
-        ++ lib.optionals (args ? nixosConfig) args.nixosConfig.environment.profiles;
-
-      binPaths = lib.pipe paths [
-        (builtins.map (p: "${p}/bin"))
-        (builtins.map (
-          builtins.replaceStrings
-            [ "$USER" "$HOME" "\${XDG_STATE_HOME}" ]
-            [ config.home.username config.home.homeDirectory config.xdg.stateHome ]
-        ))
-      ];
-
-      esepDirListToList = var: ''
-        "${var}": {
-          from_string: { |s| $s | split row (char esep) | path expand --no-symlink }
-          to_string: { |v| $v | path expand --no-symlink | str join (char esep) }
-        }
-      '';
-    in
-    lib.mkBefore (
-      ''
-        ${exportToNuEnv config.home.sessionVariables}
-
-        $env.ENV_CONVERSIONS = {
-          ${esepDirListToList "TERMINFO_DIRS"}
-          ${esepDirListToList "XDG_CONFIG_DIRS"}
-          ${esepDirListToList "XDG_DATA_DIRS"}
-          ${esepDirListToList "XCURSOR_PATH"}
-        }
-      ''
-      + ''
-        $env.PATH = $env.PATH | split row (char esep) | prepend [
-          ${lib.concatStringsSep "\n" (
-            (lib.optionals (args ? nixosConfig) [ "/run/wrappers/bin" ]) ++ config.home.sessionPath ++ binPaths
-          )}
-        ] | uniq
-      ''
+      ) vars
     );
+in
+{
+  programs.nushell.extraEnv = lib.mkBefore ''
+    ${exportToNuEnv config.home.sessionVariables}
+  '';
 }
