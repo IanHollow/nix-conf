f8629776329ee1e6cd283fb4bcb79d5f4c4ea988
Ian Holloway <72767437+IanHollow@users.noreply.github.com>
2025-08-11 14:20:44 -0700

    refactor: Update Homebrew configuration to disable auto-update and enhance cask arguments

--- configs/darwin/homebrew/default.nix
+++ configs/darwin/homebrew/default.nix
@@ -35,15 +35,35 @@
     enable = true;
     global.autoUpdate = false;
     onActivation = {
-      autoUpdate = true;
+      autoUpdate = false;
       upgrade = true;
       cleanup = "zap";
     };
+
+    caskArgs = {
+      appdir = "/Applications";
+      # require_sha = true; # Uncomment if you want strict checksum checks (can be noisy)
+    };
+
+    taps = builtins.attrNames config.nix-homebrew.taps;
 
     # TODO: move this to config specific to the user or system
     casks = [
       "signal"
       "discord"
     ];
+
+    # If you truly need a formula that Nix doesn’t provide well, add here sparingly:
+    # brews = [
+    #   # "mas" # Needed below for Mac App Store management
+    #   # "gnupg"
+    # ];
+
+    # Optional: Mac App Store apps (requires 'mas' formula and that you are signed into App Store)
+    # masApps = {
+    #   "Xcode" = 497799835;
+    #   "Magnet" = 441258766;
+    #   "Slack" = 803453959;
+    # };
   };
 }
