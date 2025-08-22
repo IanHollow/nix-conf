59ac28afa126599f9afa609cd8a6618fa8782403
Ian Holloway <72767437+IanHollow@users.noreply.github.com>
2025-08-21 22:22:36 -0400

    refactor: Simplify ghostty configuration by removing unnecessary inputs and using standard package

--- configs/home/programs/ghostty.nix
+++ configs/home/programs/ghostty.nix
@@ -1,36 +1,27 @@
 {
   lib,
   pkgs,
-  inputs,
   config,
   ...
 }:
-let
-  pkgsNur =
-    (import inputs.nixpkgs {
-      inherit (pkgs) system;
-      overlays = [ inputs.nur.overlays.default ];
-    }).nur;
-in
 {
   programs.ghostty = {
     enable = true;
-    package = lib.mkIf (pkgs.stdenv.isDarwin) pkgsNur.repos.DimitarNestorov.ghostty;
+    package = lib.mkIf (pkgs.stdenv.isDarwin) pkgs.ghostty-bin;
 
-    settings =
-      {
-        background-blur-radius = 20;
-        mouse-hide-while-typing = true;
-        window-decoration = true;
+    settings = {
+      background-blur-radius = 20;
+      mouse-hide-while-typing = true;
+      window-decoration = true;
+    }
+    // lib.optionalAttrs (lib.hasAttr "SHELL" config.home.sessionVariables) (
+      let
+        shellPath = config.home.sessionVariables.SHELL;
+        shellName = lib.last (lib.splitString "/" shellPath);
+      in
+      lib.optionalAttrs (shellName == "nu") {
+        command = "${lib.getExe' pkgs.bashInteractive "bash"} --login -c '${shellPath} --login --interactive'";
       }
-      // lib.optionalAttrs (lib.hasAttr "SHELL" config.home.sessionVariables) (
-        let
-          shellPath = config.home.sessionVariables.SHELL;
-          shellName = lib.last (lib.splitString "/" shellPath);
-        in
-        lib.optionalAttrs (shellName == "nu") {
-          command = "${lib.getExe' pkgs.bashInteractive "bash"} --login -c '${shellPath} --login --interactive'";
-        }
-      );
+    );
   };
 }
