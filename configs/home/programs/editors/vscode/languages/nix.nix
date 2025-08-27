05df3462acadd86883cee7ec476e4a48095d77d5
Ian Holloway <72767437+IanHollow@users.noreply.github.com>
2025-08-26 22:52:11 -0400

    fix: Correct server path reference and update nixfmt command in vscode settings

--- configs/home/programs/editors/vscode/languages/nix.nix
+++ configs/home/programs/editors/vscode/languages/nix.nix
@@ -20,10 +20,10 @@ in
 
   programs.vscode.profiles.default.userSettings = {
     "nix.enableLanguageServer" = true;
-    "nix.serverPath" = lib.getExe pkgs.nixd; # inputs.nixd.packages.${pkgs.system}.nixd
+    "nix.serverPath" = lib.getExe pkgs.nixd; # inputs.nixd.packages.${system}.nixd
     "nix.serverSettings".nixd = {
       formatting.command = [
-        "${lib.getExe pkgs.nixfmt-rfc-style}"
+        "${lib.getExe pkgs.nixfmt}"
         "--filename={file}"
         "--strict"
       ];
