edac01639c244ba7595ddf6bd3ae41be5d04c6f9
Ian Holloway <72767437+IanHollow@users.noreply.github.com>
2025-09-08 13:51:19 -0400

    feat: add GitLab email configuration to gitEmailConfig.nix

--- homes/macbook-pro-m4/gitEmailConfig.nix
+++ homes/macbook-pro-m4/gitEmailConfig.nix
@@ -4,4 +4,5 @@
   "gist.github.com" = config.age.secrets.gitconfig-userEmail-GitHub.path;
 
   "github.coecis.cornell.edu" = config.age.secrets.gitconfig-userEmail-Cornell.path;
+  "gitlab.cs.cornell.edu" = config.age.secrets.gitconfig-userEmail-Cornell.path;
 }
