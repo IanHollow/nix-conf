9a8f1cdd01486980dbaa5acb2163b6938bef9a02
Ian Holloway <72767437+IanHollow@users.noreply.github.com>
2025-09-07 23:13:24 -0400

    feat: add dynamic email configuration for Git based on website

---
+++ homes/macbook-pro-m4/gitEmailConfig.nix
@@ -0,0 +1,7 @@
+{ config, ... }:
+{
+  "github.com" = config.age.secrets.gitconfig-userEmail-GitHub.path;
+  "gist.github.com" = config.age.secrets.gitconfig-userEmail-GitHub.path;
+
+  "github.coecis.cornell.edu" = config.age.secrets.gitconfig-userEmail-Cornell.path;
+}
