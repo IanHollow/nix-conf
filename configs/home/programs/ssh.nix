ecd5b78b4035d2211a69ac5377850a601d46fff0
Ian Holloway <72767437+IanHollow@users.noreply.github.com>
2025-09-09 15:51:15 -0400

    feat: enhance SSH configuration with additional options and server definitions

--- configs/home/programs/ssh.nix
+++ configs/home/programs/ssh.nix
@@ -14,35 +14,36 @@ in
     enableDefaultConfig = false;
 
     matchBlocks = {
-      "*" = {
-        # Fast, reliable connections
-        controlMaster = "auto";
-        controlPersist = "10m";
-        controlPath = "${cmDir}/%C"; # hashed path avoids 'too long' errors
-        compression = true; # good on slow/latency links
+      "*" = lib.mkMerge [
+        {
+          # Fast, reliable connections
+          controlMaster = "auto";
+          controlPersist = "10m";
+          controlPath = "${cmDir}/%C"; # hashed path avoids 'too long' errors
+          compression = true; # good on slow/latency links
 
-        # Safety + convenience
-        hashKnownHosts = true;
-        addKeysToAgent = "yes";
-        forwardAgent = lib.mkForce false; # only enable per-host when needed
-        serverAliveInterval = 60;
-        serverAliveCountMax = 3;
+          # Safety + convenience
+          hashKnownHosts = true;
+          addKeysToAgent = "yes";
+          forwardAgent = lib.mkForce false; # only enable per-host when needed
+          serverAliveInterval = 60;
+          serverAliveCountMax = 3;
 
-        # TODO: fix below
-        # Accept host key updates for hosts you already trust (key rotation)
-        # UpdateHostKeys = "yes";
+          # safer first-connection behavior + key rotation
+          extraOptions = {
+            UpdateHostKeys = "yes";
+            StrictHostKeyChecking = "accept-new";
+          };
+        }
 
-        # Small QoL
-        # TCPKeepAlive = "yes";
-        # EscapeChar = "none";
-        # VisualHostKey = "yes";
-      }
-      // lib.optionalAttrs isDarwin {
-        # TODO: fix below
-        # macOS: Use the system keychain for private keys
-        # IgnoreUnknown = "UseKeychain";
-        # UseKeychain = "yes";
-      };
+        (lib.optionalAttrs isDarwin {
+          # macOS: Use the system keychain for private keys
+          extraOptions = {
+            "IgnoreUnknown" = "UseKeychain";
+            "UseKeychain" = "yes";
+          };
+        })
+      ];
 
       "github.com" = {
         hostname = "github.com";
--- homes/macbook-pro-m4/ssh.nix
+++ homes/macbook-pro-m4/ssh.nix
@@ -1,13 +1,29 @@
 { config, ... }:
 {
   programs.ssh.matchBlocks = {
-    ugclinux = {
+    # Servers
+    "ugclinux" = {
       hostname = "ugclinux.cs.cornell.edu";
 
       extraOptions = {
         Include = config.age.secrets.cornell-net-id-ssh-config.path;
         SetEnv = "TERM=xterm-256color";
       };
+    };
+
+    # Git
+    "github.coecis.cornell.edu" = {
+      hostname = "github.coecis.cornell.edu";
+      user = "git";
+      identitiesOnly = true;
+      identityFile = [ "${config.home.homeDirectory}/.ssh/id_ed25519" ];
+    };
+
+    "gitlab.cs.cornell.edu" = {
+      hostname = "gitlab.cs.cornell.edu";
+      user = "git";
+      identitiesOnly = true;
+      identityFile = [ "${config.home.homeDirectory}/.ssh/id_ed25519" ];
     };
   };
 }
