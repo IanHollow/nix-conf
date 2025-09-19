10d668b67cd9626cb1e9adbaa43511151d12a5a8
Ian Holloway <72767437+IanHollow@users.noreply.github.com>
2025-09-19 00:51:13 -0400

    feat: implement args.nix for system package configuration

---
+++ parts/args.nix
@@ -0,0 +1,16 @@
+{ inputs, ... }:
+{
+  systems = import inputs.systems;
+
+  perSystem =
+    { system, ... }:
+    {
+      _module.args.pkgs = import inputs.nixpkgs {
+        inherit system;
+        config = {
+          allowUnfree = true;
+          allowUnsupportedSystem = true;
+        };
+      };
+    };
+}
