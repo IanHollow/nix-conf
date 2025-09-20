5a46cfa389fb6422e8246e9bfb5bf9469c3888b7
Ian Holloway <72767437+IanHollow@users.noreply.github.com>
2025-09-19 21:58:03 -0400

    fix: enable additional settings for deadnix, mixed-line-endings, shfmt, and markdownlint

--- parts/hooks.nix
+++ parts/hooks.nix
@@ -2,19 +2,24 @@
   nixfmt-rfc-style.enable = true;
   statix.enable = true;
   deadnix.enable = true;
+  deadnix.settings.edit = true;
   nil.enable = true;
 
   ripsecrets.enable = true;
   end-of-file-fixer.enable = true;
   trim-trailing-whitespace.enable = true;
   mixed-line-endings.enable = true;
+  mixed-line-endings.args = [ "--fix=lf" ];
   check-merge-conflicts.enable = true;
   check-symlinks.enable = true;
 
   actionlint.enable = true;
   shellcheck.enable = true;
   shfmt.enable = true;
+  shfmt.settings.simplify = true;
 
   markdownlint.enable = true;
+  markdownlint.args = [ "--fix" ];
+  prettier.enable = true;
   yamllint.enable = true;
 }
