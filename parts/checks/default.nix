0cc28ce431b06d7fabce495792e06f741498c6e7
Ian Holloway <72767437+IanHollow@users.noreply.github.com>
2025-09-19 21:48:21 -0400

    refactor: reorganize git-hooks and checks structure for improved clarity

--- parts/checks/default.nix
+++ parts/checks/default.nix
@@ -1,21 +1,29 @@
+{ self, ... }:
 {
   perSystem =
-    { pkgs, ... }:
+    { pkgs, system, ... }:
     {
-      checks.default =
-        let
-          name = "check-store-errors";
-          hashName = builtins.substring 0 32 (builtins.hashString "sha256" name);
-        in
-        pkgs.writeShellApplication {
-          inherit name;
-          text = ''
-            while nix flake check --no-build |& grep "is not valid" >/tmp/invalid-${hashName}; do
-              path=$(</tmp/invalid-${hashName} awk -F\' '{print $2}')
-              echo "Repairing $path"
-              sudo nix-store --repair-path "$path" >/dev/null
-            done
-          '';
+      checks = {
+        default =
+          let
+            name = "check-store-errors";
+            hashName = builtins.substring 0 32 (builtins.hashString "sha256" name);
+          in
+          pkgs.writeShellApplication {
+            inherit name;
+            text = ''
+              while nix flake check --no-build |& grep "is not valid" >/tmp/invalid-${hashName}; do
+                path=$(</tmp/invalid-${hashName} awk -F\' '{print $2}')
+                echo "Repairing $path"
+                sudo nix-store --repair-path "$path" >/dev/null
+              done
+            '';
+          };
+
+        pre-commit-check = self.inputs.git-hooks.lib.${system}.run {
+          src = ./../../.;
+          hooks = import ./../hooks.nix;
         };
+      };
     };
 }
