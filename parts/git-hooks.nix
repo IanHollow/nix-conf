1d93fcbf5872d91101067d37272567133ccc37f2
Ian Holloway <72767437+IanHollow@users.noreply.github.com>
2025-09-19 21:12:10 -0400

    refactor: streamline git-hooks configuration by consolidating hook definitions

--- parts/git-hooks.nix
+++ parts/git-hooks.nix
@@ -1,38 +1,33 @@
 { self, ... }:
-{
-  flake = {
-    git-hooks.hooks = {
-      nixfmt-rfc-style.enable = true;
-      statix.enable = true; # static analysis for common pitfalls
-      deadnix.enable = true; # finds unused let bindings, etc.
+let
+  hooks = {
+    nixfmt-rfc-style.enable = true;
+    statix.enable = true;
+    deadnix.enable = true;
+    nil.enable = true;
 
-      nil.enable = true; # Nix language server checks
+    ripsecrets.enable = true;
+    end-of-file-fixer.enable = true;
+    trim-trailing-whitespace.enable = true;
+    mixed-line-endings.enable = true;
+    check-merge-conflicts.enable = true;
+    check-symlinks.enable = true;
 
-      # Secrets & hygiene
-      ripsecrets.enable = true;
-      end-of-file-fixer.enable = true;
-      trim-trailing-whitespace.enable = true;
-      mixed-line-endings.enable = true;
-      check-merge-conflicts.enable = true;
-      check-symlinks.enable = true;
+    actionlint.enable = true;
+    shellcheck.enable = true;
+    shfmt.enable = true;
 
-      # Markdown/YAML
-      markdownlint.enable = true;
-      yamllint.enable = true;
-
-      # Optional: disallow direct commits to main
-      # no-commit-to-branch.settings.branch = [ "main" "master" ];
-      # no-commit-to-branch.enable = true;
-    };
+    markdownlint.enable = true;
+    yamllint.enable = true;
   };
-
+in
+{
   perSystem =
     { system, ... }:
     {
-
       checks.pre-commit-check = self.inputs.git-hooks.lib.${system}.run {
         src = ./.;
-        inherit (self.git-hooks) hooks;
+        inherit hooks;
       };
     };
 }
