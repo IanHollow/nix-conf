485df5d1783c3a2593ed1e5ec6918f29ffe74216
Ian Holloway <72767437+IanHollow@users.noreply.github.com>
2025-09-22 19:53:22 -0400

    add start to vscode updater

--- pkgs/vscode-extensions/copilot/default.nix
+++ pkgs/vscode-extensions/copilot/default.nix
@@ -3,8 +3,8 @@ vscode-utils.buildVscodeMarketplaceExtension {
   mktplcRef = {
     publisher = "github";
     name = "copilot";
-    version = "1.372.0";
-    hash = "sha256-1L4zE2waIjI1Z8hYFaeHbnSWX9g31Sre4uDNOiQ2Fz8=";
+    version = "1.373.1788";
+    hash = "sha256-l12UNAF5Nk8hyzLw/AL08I6mAF/fJDHa0mvvD99StbE=";
   };
 
   meta = {
