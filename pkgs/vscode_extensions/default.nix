1662dacbb96ec8e7906cc25c387394ba45b494d3
Ian Holloway <72767437+IanHollow@users.noreply.github.com>
2025-09-27 21:19:08 -0400

    improve how packages defined

--- pkgs/vscode_extensions/default.nix
+++ pkgs/vscode_extensions/default.nix
@@ -1,5 +1,14 @@
-{ callPackage, ... }:
-{
-  copilot = callPackage ./copilot { };
-  copilot-chat = callPackage ./copilot-chat { };
-}
+{ callPackage, lib, ... }:
+
+let
+  dirPath = ./.;
+  entries = builtins.readDir dirPath;
+  packageDirs = lib.filterAttrs (
+    dirName: type: type == "directory" && builtins.pathExists (dirPath + "/${dirName}/default.nix")
+  ) entries;
+  attrNameFor = dirName: lib.replaceStrings [ "_" ] [ "-" ] dirName;
+in
+lib.mapAttrs' (dirName: _: {
+  name = attrNameFor dirName;
+  value = callPackage (dirPath + "/${dirName}") { };
+}) packageDirs
