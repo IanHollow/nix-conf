baec519cf6620b0cf773ae889b46e7098b122322
Ian Holloway <72767437+IanHollow@users.noreply.github.com>
2025-10-17 23:00:56 -0400

    add terminal profiles configuration for VSCode and integrate Copilot settings

--- configs/home/programs/editors/vscode/settings.nix
+++ configs/home/programs/editors/vscode/settings.nix
@@ -7,6 +7,7 @@ profileName:
 }@args:
 let
   extensions = pkgs.callPackage ./marketplace.nix args;
+  terminalProfiles = pkgs.callPackage ./terminalProfiles.nix args;
 in
 {
   programs.vscode.profiles.${profileName} = {
@@ -228,39 +229,8 @@ in
         "terminal.integrated.localEchoEnabled" = "off";
       }
 
-      (lib.mkIf (lib.hasAttr "SHELL" config.home.sessionVariables) (
-        let
-          shellPath = config.home.sessionVariables.SHELL;
-          shellName = lib.last (lib.splitString "/" shellPath);
-          os = if pkgs.stdenv.isLinux then "linux" else "osx";
-        in
-        {
-          # Define extra shells
-          "terminal.integrated.profiles.${os}" = {
-            ${shellName} = {
-              path = shellPath;
-              overrideName = true;
-              icon =
-                if shellName == "bash" then
-                  "terminal-bash"
-                else if shellName == "nu" then
-                  "chevron-right"
-                else
-                  "terminal";
-            }
-            // lib.optionalAttrs (shellName == "nu") { args = [ "--login --interactive" ]; };
-          };
-
-          # set the integrated terminal to use SHELL so make sure SHELL is set correctly
-          "terminal.integrated.defaultProfile.${os}" = shellName;
-
-          # set the default shell for automation tasks to a fully POSIX compliant shell
-          "terminal.integrated.automationProfile.${os}" = {
-            "path" = "${lib.getExe' pkgs.bashInteractive "sh"}";
-            "args" = [ "--login" ];
-          };
-        }
-      ))
+      # Terminal profiles (Copilot and integrated terminal)
+      terminalProfiles
 
       # Direnv settings
       (lib.mkIf config.programs.direnv.enable {
