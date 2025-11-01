6b7a427c79f05b78797174fb449f69af24ace74e
Ian Holloway <72767437+IanHollow@users.noreply.github.com>
2025-11-01 05:26:34 -0400

    fix: switch to only using packages that work on darwin

--- configs/home/shells/macgnu.nix
+++ configs/home/shells/macgnu.nix
@@ -1,29 +1,31 @@
-{ pkgs, ... }:
+{ pkgs, lib, ... }:
 {
-  home.packages = with pkgs; [
-    coreutils
-    binutils
-    diffutils
-    findutils
-    moreutils
-    util-linux
+  home.packages = lib.mkIf pkgs.stdenv.isDarwin (
+    with pkgs;
+    [
+      coreutils # TODO: consider using uutil-coreutils once more stable
+      darwin.binutils
+      diffutils # TODO: consider using uutil-diffutils once more stable
+      findutils # TODO: consider using uutil-findutils once more stable
+      util-linux
 
-    ed
-    gawk
-    gnused
-    gnutar
-    which
-    gnugrep
-    gzip
-    gnupatch
-    less
-    gnumake
+      ed
+      gawk
+      gnused
+      gnutar
+      which
+      gnugrep
+      gzip
+      gnupatch
+      less
+      gnumake
 
-    procps
-    file
-    libiconv
-    curl
-    wget
-    tree
-  ];
+      procps
+      file
+      libiconv
+      curl
+      wget
+      tree
+    ]
+  );
 }
