8d896d3ca040da0615695c19b803b0606cdfe910
IanHollow <72767437+IanHollow@users.noreply.github.com>
2025-11-03 06:18:04 +0000

    packages: update sources
    Co-authored-by: Ian Holloway <72767437+IanHollow@users.noreply.github.com>

--- pkgs/arkenfox_user_js/default.nix
+++ pkgs/arkenfox_user_js/default.nix
@@ -10,10 +10,10 @@
   nix,
 }:
 let
-  version = "140.0";
+  version = "140.1";
   userJsSrc = builtins.fetchurl {
     url = "https://raw.githubusercontent.com/arkenfox/user.js/${version}/user.js";
-    sha256 = "sha256-/cz0dnQXKa3c/DqUTAEwBV0I9Tc3x6uzU6rtYijg3Zo=";
+    sha256 = "sha256-jxzIiARi+GXD+GSGPr1exeEHjR/LsXSUQPGZ+hF36xg=";
   };
   updateScriptDrv = writeShellApplication {
     name = "update-arkenfox-user-js";
