9e137f835e732f4fa5712269027a95a8974b8ce0
Ian Holloway <72767437+IanHollow@users.noreply.github.com>
2025-11-17 15:20:07 -0500

    add more configuration to ghostty

--- configs/home/programs/ghostty.nix
+++ configs/home/programs/ghostty.nix
@@ -5,7 +5,7 @@
   ...
 }:
 let
-  inherit (pkgs.stdenv.hostPlatform) isDarwin;
+  inherit (pkgs.stdenv.hostPlatform) isDarwin isLinux;
 in
 {
   programs.ghostty = {
@@ -15,7 +15,12 @@ in
     settings = {
       background-blur-radius = 20;
       mouse-hide-while-typing = true;
-      window-decoration = isDarwin;
+      window-decoration = lib.mkIf isLinux false;
+
+      shell-integration = false; # home-manager handles this
+      shell-integration-features = true;
+
+      auto-update = false;
     }
     // lib.optionalAttrs (builtins.hasAttr "stylix" config) {
       font-size = lib.mkForce (config.stylix.fonts.sizes.terminal * 4.0 / 3.0);
