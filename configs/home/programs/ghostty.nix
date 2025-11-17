7368963f112064cac5e10c443782e70b52fb8afc
Ian Holloway <72767437+IanHollow@users.noreply.github.com>
2025-11-17 15:22:22 -0500

    fix(ghostty): update shell integration setting to "none"

--- configs/home/programs/ghostty.nix
+++ configs/home/programs/ghostty.nix
@@ -12,12 +12,13 @@ in
     enable = true;
     package = lib.mkIf isDarwin pkgs.ghostty-bin;
 
+    # DOCS: https://ghostty.org/docs/config/reference
     settings = {
       background-blur-radius = 20;
       mouse-hide-while-typing = true;
       window-decoration = lib.mkIf isLinux false;
 
-      shell-integration = false; # home-manager handles this
+      shell-integration = "none"; # home-manager handles this
       shell-integration-features = true;
 
       auto-update = false;
