ce723e78b2a753cb78ed50e38e4ce0a7188d3b17
Ian Holloway <72767437+IanHollow@users.noreply.github.com>
2025-11-18 12:54:05 -0500

    vscode: add better checking for git settings

--- configs/home/programs/editors/vscode/settings.nix
+++ configs/home/programs/editors/vscode/settings.nix
@@ -165,7 +165,9 @@ in
               "/etc/profiles/per-user/${config.home.username}/bin/git"
             else
               lib.getExe pkgs.gitMinimal;
-          "git.enableCommitSigning" = lib.mkIf config.programs.git.signing.signByDefault true;
+          "git.enableCommitSigning" = lib.mkIf (
+            config.programs.git.enable && config.programs.git.signing.signByDefault
+          ) true;
           "githubIssues.issueCompletions.enabled" = false;
 
           # prevent pollute history with whitespace changes
