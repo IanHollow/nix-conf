4682ccca0954cb64bb630288aa8f72182d5d7610
Ian Holloway <72767437+IanHollow@users.noreply.github.com>
2025-11-30 23:13:44 -0500

    comment out garnix binary cache entries in cache.nix as there are issues with connection

--- hosts/macbook-pro-m4/cache.nix
+++ hosts/macbook-pro-m4/cache.nix
@@ -5,14 +5,14 @@ let
       "https://cache.nixos.org" # official nix cache
       "https://nix-community.cachix.org" # nix-community cache
       "https://nixpkgs-unfree.cachix.org" # unfree-package cache
-      "https://cache.garnix.io" # garnix binary cache
+      # "https://cache.garnix.io" # garnix binary cache
     ];
 
     extra-trusted-public-keys = [
       "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
       "nixpkgs-unfree.cachix.org-1:hqvoInulhbV4nJ9yJOEr+4wxhDV4xq2d1DK7S6Nj6rs="
-      "cache.garnix.io:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g="
+      # "cache.garnix.io:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g="
     ];
   };
 in
