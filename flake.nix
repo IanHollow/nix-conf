0407a1a175b46f71f30c5531e2bb0828ee7ce48a
Ian Holloway <72767437+IanHollow@users.noreply.github.com>
2025-12-19 23:20:48 -0800

    cleanup: remove unused nixConfig settings for extra substituters and trusted public keys

--- flake.nix
+++ flake.nix
@@ -314,30 +314,4 @@
 
     nixos-hardware.url = "github:NixOS/nixos-hardware";
   };
-
-  nixConfig = {
-    extra-substituters = [
-      "https://cache.nixos.org" # official nix cache
-      "https://nix-community.cachix.org" # nix-community cache
-      "https://nixpkgs-unfree.cachix.org" # unfree-package cache
-      "https://cache.garnix.io" # garnix binary cache
-      "https://geonix.cachix.org" # geospatial nix
-      "https://nix-gaming.cachix.org" # nix-gaming cache
-      "https://cosmic.cachix.org" # cosmic desktop
-      "https://hyprland.cachix.org" # hyprland cache
-      "https://nixpkgs-wayland.cachix.org" # nixpkgs wayland cache
-    ];
-
-    extra-trusted-public-keys = [
-      "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
-      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
-      "nixpkgs-unfree.cachix.org-1:hqvoInulhbV4nJ9yJOEr+4wxhDV4xq2d1DK7S6Nj6rs="
-      "cache.garnix.io:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g="
-      "geonix.cachix.org-1:iyhIXkDLYLXbMhL3X3qOLBtRF8HEyAbhPXjjPeYsCl0="
-      "nix-gaming.cachix.org-1:nbjlureqMbRAxR1gJ/f3hxemL9svXaZF/Ees8vCUUs4="
-      "cosmic.cachix.org-1:Dya9IyXD4xdBehWjrkPv6rtxpmMdRel02smYzA85dPE="
-      "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
-      "nixpkgs-wayland.cachix.org-1:3lwxaILxMRkVhehr5StQprHdEo4IrE8sRho9R9HOLYA="
-    ];
-  };
 }
