409c4233b9a3b219af4fd8c18717ce7a2efaa9fa
Ian Holloway <72767437+IanHollow@users.noreply.github.com>
2026-01-21 18:15:53 -0500

    enable various ai features

--- configs/home/programs/editors/vscode/ai/models.nix
+++ configs/home/programs/editors/vscode/ai/models.nix
@@ -17,20 +17,32 @@ in
     ++ (with extensions.preferNixpkgsThenPreRelease; [
       ## Codex
       openai.chatgpt
+
+      ## Gemini
+      google.geminicodeassist
+
+      ## Anthropic
+      anthropic.claude-code
     ]);
 
     userSettings = {
       ## Copilot
       # Base
+      "chat.agent.maxRequests" = 50;
       "chat.agent.enabled" = true;
       "github.copilot.nextEditSuggestions.enabled" = false; # this is annoying
+      "chat.viewSessions.orientation" = "stacked";
 
       # Preview
       "github.copilot.chat.codesearch.enabled" = true;
 
       # Experimental
-      "github.copilot.chat.agent.thinkingTool" = true;
-      "chat.todoListTool.enabled" = true;
+      "github.copilot.chat.anthropic.tools.websearch.enabled" = true;
+      "github.copilot.chat.anthropic.tools.websearch.maxUses" = 20;
+      "github.copilot.chat.anthropic.thinking.budgetTokens" = 32000;
+      "inlineChat.notebookAgent" = true;
+      "github.copilot.chat.notebook.enhancedNextEditSuggestions.enabled" = true;
+      "github.copilot.chat.notebook.followCellExecution.enabled" = true;
     };
   };
 }
