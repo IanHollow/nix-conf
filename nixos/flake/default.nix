9a22d79f2508e1eac52873f2097550a4e0bb33b6
Ian Holloway <72767437+IanHollow@users.noreply.github.com>
2026-01-26 20:54:27 -0500

    Add partitions for nixos, darwin, and home configurations

---
+++ nixos/flake/default.nix
@@ -0,0 +1,11 @@
+# NixOS-specific configuration module
+{ inputs, ... }:
+{
+  # This is where you'll define nixosConfigurations
+  # Example:
+  # flake.nixosConfigurations = {
+  #   myhost = inputs.nixpkgs.lib.nixosSystem {
+  #     modules = [ ./hosts/myhost ];
+  #   };
+  # };
+}
