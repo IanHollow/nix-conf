7f236543c95b1a8bcdad5d220a9c71389ad6180b
Ian Holloway <72767437+IanHollow@users.noreply.github.com>
2026-02-01 17:45:14 -0500

    reorganize the file structure

---
+++ flake/dev/flake.nix
@@ -0,0 +1,15 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable-small";
+
+    git-hooks-nix = {
+      url = "github:cachix/git-hooks.nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    treefmt-nix = {
+      url = "github:numtide/treefmt-nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+  outputs = _: { };
+}
