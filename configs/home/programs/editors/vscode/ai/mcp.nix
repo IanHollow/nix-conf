dc5e328caef6644627730fbf9705ff1a1b8358db
Ian Holloway <72767437+IanHollow@users.noreply.github.com>
2026-02-04 12:50:12 -0500

    remove unused vscode things

--- configs/home/programs/editors/vscode/ai/mcp.nix
+++ configs/home/programs/editors/vscode/ai/mcp.nix
@@ -6,19 +6,6 @@ profileName: _: {
         "type" = "http";
         "url" = "https://api.githubcopilot.com/mcp/";
       };
-      # Serena MCP server
-      # "oraios/serena" = {
-      #   "type" = "stdio";
-      #   "command" = lib.getExe' pkgs.uv "uvx";
-      #   "args" = [
-      #     "--from"
-      #     "git+https://github.com/oraios/serena"
-      #     "serena"
-      #     "start-mcp-server"
-      #     "--context"
-      #     "ide-assistant"
-      #   ];
-      # };
     };
   };
 }
