7a83c3a1415f98928d98c66905ea08499e59cd08
Ian Holloway <72767437+IanHollow@users.noreply.github.com>
2026-02-04 22:40:21 -0500

    move configs to the correct location

--- flake/darwin/default.nix
+++ flake/darwin/default.nix
@@ -7,6 +7,10 @@
 {
   # import = [ inputs.nix-darwin.flakeModules.nix-darwin ];
 
-  flake.modules.darwin = myLib.dir.importFlatWithDirs ../../modules/darwin { sep = "-"; };
-  flake.darwinModules = config.flake.modules.darwin;
+  flake = {
+    modules.darwin = myLib.dir.importFlatWithDirs ../../modules/darwin { sep = "-"; };
+    darwinModules = config.flake.modules.darwin;
+
+    # darwinConfigurations = ;
+  };
 }
