2bac27f7c5315f559f5c0f26b9cdb4cf086ad569
Ian Holloway <72767437+IanHollow@users.noreply.github.com>
2026-02-13 03:01:07 -0500

    experimental is broken

--- modules/home/firefox/blocking.nix
+++ modules/home/firefox/blocking.nix
@@ -143,7 +143,7 @@ in
         "ublock-privacy"
         "ublock-quick-fixes"
         "ublock-unbreak"
-        "ublock-experimental"
+        # "ublock-experimental"
 
         # Ads
         "easylist"
