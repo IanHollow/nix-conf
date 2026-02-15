612f4eac0807dd57608cf6637d9dc28f19551a76
Ian Holloway <72767437+IanHollow@users.noreply.github.com>
2026-02-15 01:12:55 -0500

    enhance bash configuration with additional options and history settings

--- modules/home/shells/bash/default.nix
+++ modules/home/shells/bash/default.nix
@@ -1,8 +1,41 @@
+{ lib, pkgs, ... }:
+let
+  inherit (pkgs.stdenv.hostPlatform) isLinux;
+in
 {
   home.shell.enableBashIntegration = true;
   programs.bash = {
     enable = true;
-
     enableCompletion = true;
+    enableVteIntegration = lib.mkIf isLinux true;
+
+    historyControl = [
+      "ignoreboth"
+      "erasedups"
+    ];
+    historyIgnore = [
+      "exit"
+      "clear"
+    ];
+    historySize = 100000;
+    historyFileSize = 200000;
+
+    shellOptions = [
+      "histappend"
+      "checkwinsize"
+      "extglob"
+      "globstar"
+      "cmdhist"
+    ];
+
+    initExtra = lib.mkMerge [
+      (lib.mkOrder 500 ''
+        source -- "${pkgs.blesh}/share/blesh/ble.sh" --attach=none
+      '')
+
+      (lib.mkOrder 2000 ''
+        [[ ! ''${BLE_VERSION-} ]] || ble-attach
+      '')
+    ];
   };
 }
