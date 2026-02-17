bc79ab7cdd50050f489048fc00fbae8a0d27a043
Ian Holloway <72767437+IanHollow@users.noreply.github.com>
2026-02-16 20:37:18 -0500

    remove old code

--- old/lib/files/default.nix
+++
@@ -1,9 +0,0 @@
-{ lib, ... }:
-{
-  mkDirEntry = import ./mkDirEntry.nix { inherit lib; };
-  mkDirEntries = import ./mkDirEntries.nix { inherit lib; };
-  nixDirEntries = import ./nixDirEntries.nix { inherit lib; };
-  importRec = import ./importRec.nix { inherit lib; };
-  importDirRec = import ./importDirRec.nix { inherit lib; };
-  importDirFlat = import ./importDirFlat.nix { inherit lib; };
-}
--- old/lib/withTreeModules.nix
+++
@@ -1,4 +0,0 @@
-modules:
-builtins.map (
-  x: if (builtins.typeOf x == "set" && builtins.hasAttr "default" x) then x.default else x
-) modules
