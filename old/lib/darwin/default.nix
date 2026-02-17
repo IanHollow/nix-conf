a7ed49b27787c35aed48982dca548a0f6800bc56
Ian Holloway <72767437+IanHollow@users.noreply.github.com>
2026-02-16 20:37:18 -0500

    remove old code

--- old/lib/darwin/default.nix
+++
@@ -1,5 +0,0 @@
-{
-  # Functions in darwin folder create configs for nix-dawrin in the form of sets
-
-  addUser = import ./addUser.nix;
-}
--- old/lib/firefox.nix
+++
@@ -1,8 +0,0 @@
-{ lib }:
-{
-  toUserJS =
-    kv:
-    lib.concatLines (
-      lib.mapAttrsToList (k: v: "user_pref(${builtins.toJSON k}, ${builtins.toJSON v});") kv
-    );
-}
