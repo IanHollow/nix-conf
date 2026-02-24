827531d6515318d69d208872e1c80a38c6e36cfa
Ian Holloway <72767437+IanHollow@users.noreply.github.com>
2026-02-24 00:14:37 -0500

    update flake

--- flake/dev/flake.nix
+++ flake/dev/flake.nix
@@ -4,11 +4,19 @@
 
     git-hooks-nix = {
       url = "github:cachix/git-hooks.nix";
-      inputs.nixpkgs.follows = "nixpkgs";
+      inputs = {
+        nixpkgs.follows = "nixpkgs";
+        flake-compat.follows = "flake-compat";
+      };
     };
     treefmt-nix = {
       url = "github:numtide/treefmt-nix";
       inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    flake-compat = {
+      url = "github:NixOS/flake-compat";
+      flake = false;
     };
   };
   outputs = _: { };
