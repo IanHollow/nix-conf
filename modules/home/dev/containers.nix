05974972ea9e0f22f3a12ab02d1bdc96507d3188
Ian Holloway <72767437+IanHollow@users.noreply.github.com>
2026-02-24 19:00:00 -0500

    switch from podman to docker

--- modules/home/dev/containers.nix
+++ modules/home/dev/containers.nix
@@ -1,23 +1,54 @@
-{ pkgs, lib, ... }:
+{
+  pkgs,
+  lib,
+  config,
+  ...
+}:
 let
-  inherit (pkgs.stdenv.hostPlatform) isDarwin isLinux;
+  inherit (pkgs.stdenv.hostPlatform) isDarwin;
 in
 {
-  home.packages = [ pkgs.podman-compose ] ++ lib.optionals isDarwin [ pkgs.container ];
+  home.packages = [
+    pkgs.docker
+    pkgs.docker-compose
+    pkgs.docker-buildx
+  ]
+  ++ lib.optionals isDarwin [ pkgs.container ];
 
-  services.podman = {
+  programs.docker-cli = {
     enable = true;
-    enableTypeChecks = isLinux;
-  }
-  // lib.optionalAttrs isDarwin {
-    machines.podman-machine-default = {
-      autoStart = false;
-      memory = 4 * 1024;
-    };
+  };
+  home.file."${config.programs.docker-cli.configDir}/cli-plugins/docker-buildx".source =
+    "${pkgs.docker-buildx}/bin/docker-buildx";
+  home.file."${config.programs.docker-cli.configDir}/cli-plugins/docker-compose".source =
+    "${pkgs.docker-compose}/bin/docker-compose";
+
+  programs.lazydocker = {
+    enable = true;
   };
 
-  home.shellAliases = {
-    docker = "podman";
-    docker-compose = "podman-compose";
+  services.colima = {
+    enable = isDarwin;
+
+    profiles.default = {
+      isService = true;
+      isActive = true;
+
+      settings = {
+        runtime = "docker";
+        arch = "host";
+
+        vmType = "vz";
+        mountType = "virtiofs";
+
+        cpu = 4;
+        memory = 4;
+
+        kubernetes.enabled = false;
+
+        portForwarder = "ssh";
+        autoActivate = false;
+      };
+    };
   };
 }
