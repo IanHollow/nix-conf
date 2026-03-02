5b8f60729eef22c7bcce9b8aa01aaee8d204aefb
Ian Holloway <72767437+IanHollow@users.noreply.github.com>
2026-03-02 14:10:42 -0500

    update flake

--- modules/home/discord.nix
+++ modules/home/discord.nix
@@ -1,24 +1,10 @@
-{ lib, pkgs, ... }:
+{ pkgs, ... }:
 let
   inherit (pkgs.stdenv.hostPlatform) isLinux;
-
-  vesktopPackage =
-    if pkgs.stdenv.hostPlatform.isDarwin then
-      pkgs.vesktop.overrideAttrs (old: {
-        # Explicitly disable macOS signing in electron-builder for arm64-darwin.
-        buildPhase =
-          lib.replaceStrings
-            [ "  -c.electronDist=. \\\n" ]
-            [ "  -c.mac.identity=null \\\n  -c.electronDist=. \\\n" ]
-            old.buildPhase;
-      })
-    else
-      pkgs.vesktop;
 in
 {
   programs.vesktop = {
     enable = true;
-    package = vesktopPackage;
     vencord = {
       useSystem = true;
       settings = { };
