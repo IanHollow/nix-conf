d8d625c91f60ba46cbaa11c7b7a8a4a08cb5f40e
Ian Holloway <72767437+IanHollow@users.noreply.github.com>
2026-03-10 20:28:40 -0400

    fix: update homeDirectory references in xdg configuration

--- modules/home/xdg/default.nix
+++ modules/home/xdg/default.nix
@@ -1,19 +1,32 @@
-{ config, ... }:
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
+let
+  inherit (pkgs.stdenv.hostPlatform) isDarwin;
+  inherit (config.home) homeDirectory;
+in
 {
   xdg = {
     enable = true;
 
-    configHome = "${config.home.homeDirectory}/.config";
-    dataHome = "${config.home.homeDirectory}/.local/share";
-    stateHome = "${config.home.homeDirectory}/.local/state";
-    cacheHome = "${config.home.homeDirectory}/.cache";
+    configHome = "${homeDirectory}/.config";
+    dataHome = "${homeDirectory}/.local/share";
+    stateHome = "${homeDirectory}/.local/state";
+    cacheHome = "${homeDirectory}/.cache";
 
     userDirs = {
       enable = true;
       createDirectories = true;
       extraConfig = {
-        PROJECTS = "${config.home.homeDirectory}/Projects";
+        PROJECTS = "${homeDirectory}/Projects";
+        SCREENSHOTS = "${config.xdg.userDirs.pictures}/Screenshots";
       };
+      videos = lib.mkIf isDarwin (lib.mkDefault "${homeDirectory}/Movies");
+      publicShare = lib.mkIf isDarwin (lib.mkDefault null);
+      templates = lib.mkIf isDarwin (lib.mkDefault null);
     };
   };
 
