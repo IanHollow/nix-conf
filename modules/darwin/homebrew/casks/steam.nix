12af7f464267b2e89a79dbbe54f25fbba02bb458
Ian Holloway <72767437+IanHollow@users.noreply.github.com>
2026-03-14 05:01:23 -0400

    add WIP steam pkg for darwin

--- modules/darwin/homebrew/casks/steam.nix
+++
@@ -1,1 +0,0 @@
-_: { homebrew.casks = [ "steam" ]; }
--- pkgs/default.nix
+++ pkgs/default.nix
@@ -2,5 +2,6 @@
 {
   claude-desktop = pkgs.callPackage ./claude-desktop { };
   codex-app = pkgs.callPackage ./codex-app { };
+  steam = pkgs.callPackage ./steam { };
   ttf-ms-win11-auto = pkgs.callPackage ./ttf-ms-win11-auto { };
 }
