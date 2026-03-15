d041b9cc62ce7a73c63d3d8195484abd033ee943
Ian Holloway <72767437+IanHollow@users.noreply.github.com>
2026-03-15 16:15:34 -0400

    use hm managed config for opencode

--- modules/home/dev/agentic-tui/opencode.nix
+++ modules/home/dev/agentic-tui/opencode.nix
@@ -1,4 +1,22 @@
 { pkgs, ... }:
 {
-  home.packages = [ pkgs.opencode ];
+  programs.opencode = {
+    enable = true;
+    settings = {
+      autoupdate = false;
+      plugin = [
+        "opencode-gemini-auth@latest"
+      ];
+    };
+  };
+
+  home.sessionVariables = {
+    OPENCODE_EXPERIMENTAL_LSP_TOOLS = 1;
+    OPENCODE_EXPERIMENTAL_LSP_TY = 1;
+
+    OPENCODE_ENABLE_EXA = 1;
+    OPENCODE_EXPERIMENTAL_EXA = 1;
+
+    OPENCODE_EXPERIMENTAL_PLAN_MODE = 1;
+  };
 }
