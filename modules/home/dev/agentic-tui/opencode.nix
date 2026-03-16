2c123a9920e0f5bd15c8b86b12800a6cbf447fcb
Ian Holloway <72767437+IanHollow@users.noreply.github.com>
2026-03-16 12:44:01 -0400

    improve opencode config still a WIP

--- modules/home/dev/agentic-tui/opencode.nix
+++ modules/home/dev/agentic-tui/opencode.nix
@@ -1,18 +1,31 @@
-{ pkgs, ... }:
-{
+_: {
   programs.opencode = {
     enable = true;
     settings = {
       autoupdate = false;
-      plugin = [
-        "opencode-gemini-auth@latest"
-      ];
+      plugin = [ "opencode-gemini-auth@latest" "@simonwjackson/opencode-direnv" ];
+      lsp = {
+        pyright.disabled = true;
+        ty = {
+          command = [
+            "uv"
+            "run"
+            "ty"
+            "server"
+          ];
+          extensions = [
+            ".py"
+            ".pyi"
+          ];
+        };
+      };
     };
   };
 
   home.sessionVariables = {
     OPENCODE_EXPERIMENTAL_LSP_TOOLS = 1;
     OPENCODE_EXPERIMENTAL_LSP_TY = 1;
+    OPENCODE_DISABLE_LSP_DOWNLOAD = 1;
 
     OPENCODE_ENABLE_EXA = 1;
     OPENCODE_EXPERIMENTAL_EXA = 1;
