ae5fbdb3bfdae940b54a6dad7f5f4d08df1f37f7
Ian Holloway <72767437+IanHollow@users.noreply.github.com>
2026-03-23 19:39:55 -0400

    update flake

--- modules/home/dev/direnv.nix
+++ modules/home/dev/direnv.nix
@@ -1,10 +1,15 @@
 { pkgs, ... }:
+let
+  direnvPkg = pkgs.direnv.overrideAttrs {
+    postPatch = ''
+      substituteInPlace GNUmakefile --replace-fail " -linkmode=external" ""
+    '';
+  };
+in
 {
   programs.direnv = {
     enable = true;
     nix-direnv.enable = true;
-    package = pkgs.direnv.overrideAttrs (_: {
-      doCheck = false;
-    });
+    package = direnvPkg;
   };
 }
