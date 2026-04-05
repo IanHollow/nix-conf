4aec749d4b281b927b45eb479e9cb9d9b850cf22
Ian Holloway <72767437+IanHollow@users.noreply.github.com>
2026-04-05 13:50:12 -0400

    feat: add playwright-driver to home packages and configure session variables

--- modules/home/dev/languages/js.nix
+++ modules/home/dev/languages/js.nix
@@ -1,4 +1,13 @@
 { pkgs, ... }:
 {
-  home.packages = with pkgs; [ bun ];
+  home.packages = with pkgs; [
+    bun
+
+    playwright-driver
+  ];
+
+  home.sessionVariables = {
+    PLAYWRIGHT_BROWSERS_PATH = pkgs.playwright.browsers;
+    PLAYWRIGHT_SKIP_BROWSER_DOWNLOAD = 1;
+  };
 }
