1259e4e4e44928632d535a1485012b36c704c2fe
Ian Holloway <72767437+IanHollow@users.noreply.github.com>
2026-05-09 22:59:21 -0400

    fix: update WhatsApp entry in dock configuration to use correct application path

--- configs/home/macbook-pro-m4/modules/extra-config/dock.nix
+++ configs/home/macbook-pro-m4/modules/extra-config/dock.nix
@@ -15,7 +15,7 @@
       { spacer.small = true; }
 
       { app = "/System/Applications/Messages.app"; }
-      { hmApp = "WhatsApp"; }
+      { app = "/Applications/‎WhatsApp.app"; }
       { app = "/System/Applications/Phone.app"; }
       { app = "/System/Applications/FaceTime.app"; }
       { hmApp = "Vesktop"; }
