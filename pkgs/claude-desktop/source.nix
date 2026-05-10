079028d968059752145b8b2af7d0e9f711e7d273
Ian Holloway <72767437+IanHollow@users.noreply.github.com>
2026-05-10 14:00:06 -0400

    update flake

--- pkgs/claude-desktop/source.nix
+++ pkgs/claude-desktop/source.nix
@@ -1,7 +1,7 @@
 {
-  version = "1.6608.1";
+  version = "1.6608.2";
   src = {
-    url = "https://downloads.claude.ai/releases/darwin/universal/1.6608.1/Claude-f156d01489166df990fe362e0a219bf5099a1857.zip";
-    hash = "sha256-H1/63GE14TiLoAvjMGVUVHvlbGwdsrvNd0f3ucqwhCA=";
+    url = "https://downloads.claude.ai/releases/darwin/universal/1.6608.2/Claude-ebf1a166e82541b54229aa620d117c60923a939a.zip";
+    hash = "sha256-HhG2sXV1AHISPoN9uMKwajN77mH/9NVwoDMnJFxPWpA=";
   };
 }
