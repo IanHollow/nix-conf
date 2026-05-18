f80b6cf172be03f2729c4755cce0737f2aadd884
Ian Holloway <72767437+IanHollow@users.noreply.github.com>
2026-05-18 14:02:00 -0400

    refactor: update SSH configuration for consistency and clarity

--- configs/home/desktop/ssh.nix
+++ configs/home/desktop/ssh.nix
@@ -7,54 +7,54 @@ let
       { };
 in
 {
-  programs.ssh.matchBlocks = {
+  programs.ssh.settings = {
     # Servers
     "ugclinux" = {
-      hostname = "ugclinux.cs.cornell.edu";
+      HostName = "ugclinux.cs.cornell.edu";
 
-      extraOptions = {
-        SetEnv = "TERM=xterm-256color";
-      }
-      // includeCornell;
-    };
+      SetEnv = {
+        TERM = "xterm-256color";
+      };
+    }
+    // includeCornell;
 
     # NERSC
     # https://docs.nersc.gov/connect/vscode/
     "dtn*.nersc.gov perlmutter*.nersc.gov *.nersc.gov" = {
-      identitiesOnly = true;
-      identityFile = [ "${config.home.homeDirectory}/.ssh/id_ed25519" ];
-      extraOptions = {
-        LogLevel = "QUIET";
-        SetEnv = "TERM=xterm-256color";
-      }
-      // includeCornell;
-    };
+      IdentitiesOnly = true;
+      IdentityFile = [ "${config.home.homeDirectory}/.ssh/id_ed25519" ];
+      LogLevel = "QUIET";
+      SetEnv = {
+        TERM = "xterm-256color";
+      };
+    }
+    // includeCornell;
     "nid??????" = {
-      hostname = "%h";
-      identitiesOnly = true;
-      identityFile = [ "${config.home.homeDirectory}/.ssh/id_ed25519" ];
-      extraOptions = {
-        LogLevel = "QUIET";
-        StrictHostKeyChecking = "no";
-        ProxyJump = "perlmutter.nersc.gov";
-        SetEnv = "TERM=xterm-256color";
-      }
-      // includeCornell;
-    };
+      HostName = "%h";
+      IdentitiesOnly = true;
+      IdentityFile = [ "${config.home.homeDirectory}/.ssh/id_ed25519" ];
+      LogLevel = "QUIET";
+      StrictHostKeyChecking = "no";
+      ProxyJump = "perlmutter.nersc.gov";
+      SetEnv = {
+        TERM = "xterm-256color";
+      };
+    }
+    // includeCornell;
 
     # Git
     "github.coecis.cornell.edu" = {
-      hostname = "github.coecis.cornell.edu";
-      user = "git";
-      identitiesOnly = true;
-      identityFile = [ "${config.home.homeDirectory}/.ssh/id_ed25519" ];
+      HostName = "github.coecis.cornell.edu";
+      User = "git";
+      IdentitiesOnly = true;
+      IdentityFile = [ "${config.home.homeDirectory}/.ssh/id_ed25519" ];
     };
 
     "gitlab.cs.cornell.edu" = {
-      hostname = "gitlab.cs.cornell.edu";
-      user = "git";
-      identitiesOnly = true;
-      identityFile = [ "${config.home.homeDirectory}/.ssh/id_ed25519" ];
+      HostName = "gitlab.cs.cornell.edu";
+      User = "git";
+      IdentitiesOnly = true;
+      IdentityFile = [ "${config.home.homeDirectory}/.ssh/id_ed25519" ];
     };
   };
 }
