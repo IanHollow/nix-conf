40eb7139200a3f026ffb85d4c60fcd7c734fbd89
Ian Holloway <72767437+IanHollow@users.noreply.github.com>
2026-05-29 22:30:57 -0700

    update

--- pkgs/claude-desktop/source.nix
+++ pkgs/claude-desktop/source.nix
@@ -1,7 +1,7 @@
 {
-  version = "1.9255.2";
+  version = "1.9659.2";
   src = {
-    url = "https://downloads.claude.ai/releases/darwin/universal/1.9255.2/Claude-1dc8f7b0f46a151e8522f24f6656aab10182bf92.zip";
-    hash = "sha256-hOy3X84pdOfrtdEhuAMAy6WXtC/dG7759i+FmH/pwCo=";
+    url = "https://downloads.claude.ai/releases/darwin/universal/1.9659.2/Claude-390d6ca38de808ca81aeccd2945d9c8be800f47d.zip";
+    hash = "sha256-1L1HoIAZ3m9ne1InmJwCBif+Gq+/c9YtTGShOpJpgAs=";
   };
 }
--- pkgs/codex-app/source.nix
+++ pkgs/codex-app/source.nix
@@ -1,7 +1,7 @@
 {
-  version = "26.519.81530";
+  version = "26.527.31326";
   src = {
-    url = "https://persistent.oaistatic.com/codex-app-prod/Codex-darwin-arm64-26.519.81530.zip";
-    hash = "sha256-TBFBY5Gw0S8nOXhNt8ECSB92EIiNNM8JFA8pKLUpUvQ=";
+    url = "https://persistent.oaistatic.com/codex-app-prod/Codex-darwin-arm64-26.527.31326.zip";
+    hash = "sha256-z6oU1qioqN5AMCj5J/a2S6ZpLDyXWer+wP0zdkEkudI=";
   };
 }
