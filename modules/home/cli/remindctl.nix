582dc962fe74034bcf2004ca4e91c1b26523ada1
Ian Holloway <72767437+IanHollow@users.noreply.github.com>
2026-06-04 23:26:03 -0700

    feat: add remindctl package configuration for Darwin systems

---
+++ modules/home/cli/remindctl.nix
@@ -0,0 +1,13 @@
+{
+  self,
+  system,
+  pkgs,
+  lib,
+  ...
+}:
+let
+  inherit (pkgs.stdenv.hostPlatform) isDarwin ;
+in
+{
+  home.packages = lib.mkIf isDarwin [ self.packages.${system}.remindctl ];
+}
